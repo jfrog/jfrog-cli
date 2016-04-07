547910fe903c461ea0cebaa7657cad277925802e
tamirhadad <tamirh@jfrog.com>
2016-04-07 11:44:14 +0300

    refactor the commands structure

--- jfrog/main.go
+++ jfrog/main.go
@@ -13,43 +13,22 @@ func main() {
 	app.Name = "jfrog"
 	app.Usage = "See https://github.com/jfrogdev/jfrog-cli-go for usage instructions."
 	app.Version = cliutils.GetVersion()
-
 	args := os.Args
-
-	if showFrogCommands(args) {
-		app.Commands = getCommands()
-		app.Run(args)
-	} else if args[1] == cliutils.CmdArtifactory {
-		app.Commands = artifactory.GetCommands()
-		app.Run(args[1:])
-	} else if args[1] == cliutils.CmdBintray {
-		app.Commands = bintray.GetCommands()
-		app.Run(args[1:])
-	} else {
-		cliutils.Exit(cliutils.ExitCodeError, "Unknown command " + args[1] +
-		    ". Expecting " + cliutils.CmdArtifactory + " or " + cliutils.CmdBintray + ".")
-	}
-}
-
-func showFrogCommands(args []string) bool {
-	if len(args) == 1 {
-		return true
-	}
-	if args[1] != cliutils.CmdArtifactory && args[1] != cliutils.CmdBintray {
-		return true
-	}
-	return false
+	app.Commands = getCommands()
+	app.Run(args)
 }
 
 func getCommands() []cli.Command {
 	return []cli.Command{
 		{
-			Name:  cliutils.CmdArtifactory,
-			Usage: "Artifactory commands",
+			Name:  	     cliutils.CmdArtifactory,
+			Usage:       "Artifactory commands",
+			Subcommands: artifactory.GetCommands(),
 		},
 		{
-			Name:  cliutils.CmdBintray,
-			Usage: "Bintray commands",
+			Name:        cliutils.CmdBintray,
+			Usage: 	     "Bintray commands",
+			Subcommands: bintray.GetCommands(),
 		},
 	}
 }
