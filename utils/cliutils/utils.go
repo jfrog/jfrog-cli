9db93587fc94fd2fe3380c1f0774b6c33d5f27a6
romangurevitch <romang@jfrog.com>
2016-06-06 12:23:41 +0300

    Promoted version to 1.3.0

--- utils/cliutils/utils.go
+++ utils/cliutils/utils.go
@@ -111,7 +111,7 @@ func GetLogMsgPrefix(threadId int, dryRun bool) string {
 }
 
 func GetVersion() string {
-	return "1.2.1"
+	return "1.3.0"
 }
 
 // Get the local root path, from which to start collecting artifacts to be uploaded to Artifactory.
