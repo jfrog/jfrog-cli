c1b328e6667df562f4d774bdbd2b1826dc485a66
eyalbe4 <00Rotshild00>
2017-07-22 12:51:55 +0300

    Corrected the "jfrog bt u" command help.

--- docs/bintray/upload/help.go
+++ docs/bintray/upload/help.go
@@ -11,12 +11,12 @@ const Arguments string =
 		If you have specified that you are using regular expressions, then the first one used in the argument must be enclosed in parenthesis.
 
 	target location
-		The second argument specifies the root location within the repository for upload in Bintray.
-		For flexibility in specifying the upload path, you can include placeholders in the form of {1}, {2} etc.
-		which are replaced by corresponding tokens in the source path that are enclosed in parenthesis.
+		The second argument specifies the location within Bintray for uploaded file, in the form of subject/repository/package/version.
 
 	target path
 		[Optional]
-		This third optional argument lets you specify a path under the root location to where the files should be uploaded.
+		This third optional argument lets you specify a path under the target location to where the files should be uploaded.
 		NOTE: that the path should end with a slash (/) to indicate a directory, otherwise the last element
-		in your path will be interpreted as a file name, and the file you upload will be renamed to that filename.`
+		in your path will be interpreted as a file name, and the file you upload will be renamed to that filename.
+		For flexibility in specifying the upload path, you can include placeholders in the form of {1}, {2} etc.
+		which are replaced by corresponding tokens in the source path that are enclosed in parenthesis.`
