410bb4c36a0f03bceac783662d0d0c84a5944621
Rein Achten <rein.achten@tomtom.com>
2017-08-04 21:12:06 +0200

    Removed some small inconsistencies

--- artifactory/commands/buildaddartifact.go
+++ artifactory/commands/buildaddartifact.go
@@ -32,7 +32,6 @@ func BuildAddArtifact(buildName, buildNumber, artifactPath string, flags *BuildA
 	}
 
 	log.Info("Successfully added artifact to build info")
-
 	return
 }
 
@@ -40,8 +39,7 @@ func getFileInfo(artifactPath string, flags *BuildAddArtifactFlags) (fileName st
 	getFileInfoUrl := flags.ArtDetails.Url + "api/storage/" + artifactPath
 	log.Debug("Retrieving file info through URL: " + getFileInfoUrl)
 	httpClientsDetails := utils.GetArtifactoryHttpClientDetails(flags.ArtDetails)
-	resp, body, _, err := httputils.SendGet(getFileInfoUrl, true, httpClientsDetails)
-	if err != nil {
+	resp, body, _, err := httputils.SendGet(getFileInfoUrl, true, httpClientsDetails); if err != nil {
 		return "", nil, err
 	}
 	if resp.StatusCode != 200 {
@@ -70,25 +68,24 @@ func getFileInfo(artifactPath string, flags *BuildAddArtifactFlags) (fileName st
 	return
 }
 
-func setBuildNameAndNumber(buildName, buildNumber, artifactPath string, flags *BuildAddArtifactFlags) error {
+func setBuildNameAndNumber(buildName, buildNumber, artifactPath string, flags *BuildAddArtifactFlags) (err error) {
 	log.Info("Setting build.name and build.number properties")
 
 	// Not all Artifactory versions (e.g. 4.7.5 rev 40176) seem to support setting multiple
 	// properties at once. Hence setting build.number and build.name separately.
-	err := utils.SetProps(artifactPath, "build.name=" + buildName, flags.ArtDetails); if err != nil {
-		return err
+	err = utils.SetProps(artifactPath, "build.name=" + buildName, flags.ArtDetails); if err != nil {
+		return
 	}
 	err = utils.SetProps(artifactPath, "build.number=" + buildNumber, flags.ArtDetails); if err != nil {
-		return err
+		return
 	}
-	return nil
+	return
 }
 
 func savePartialArtifactBuildInfo(buildName, buildNumber, fileName string, details *fileutils.FileDetails) error {
 	populateFunc := func(wrapper *utils.ArtifactBuildInfoWrapper) {
 		wrapper.Artifacts = []utils.ArtifactsBuildInfo{utils.CreateArtifactsBuildInfo(fileName, details)}
 	}
-
 	return utils.SavePartialBuildInfo(buildName, buildNumber, populateFunc)
 }
 
