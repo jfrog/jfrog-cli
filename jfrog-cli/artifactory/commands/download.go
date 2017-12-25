0ac3de9ea919d7666aa03cd46b53a04ca5f660a3
Alexei Vainshtein <alexeiv@jfrog.com>
2017-12-25 22:36:18 +0200

    Typo in commands/download.go

--- jfrog-cli/artifactory/commands/download.go
+++ jfrog-cli/artifactory/commands/download.go
@@ -10,7 +10,7 @@ import (
 	"github.com/jfrogdev/jfrog-cli-go/jfrog-client/artifactory/services"
 	"github.com/jfrogdev/jfrog-cli-go/jfrog-cli/artifactory/utils/buildinfo"
 	"github.com/jfrogdev/jfrog-cli-go/jfrog-cli/artifactory/utils/spec"
-	 "errors"
+	"errors"
 	"github.com/jfrogdev/jfrog-cli-go/jfrog-client/utils/log"
 )
 
@@ -34,7 +34,7 @@ func Download(downloadSpec *spec.SpecFiles, flags *DownloadConfiguration) (int, 
 	}
 	var filesInfo []clientutils.FileInfo
 	var totalExpected int
-	var isErrorOccured = false
+	var isErrorOccurred = false
 	for i := 0; i < len(downloadSpec.Files); i++ {
 		params, err := downloadSpec.Get(i).ToArtifatoryDownloadParams()
 		if err != nil {
@@ -50,13 +50,13 @@ func Download(downloadSpec *spec.SpecFiles, flags *DownloadConfiguration) (int, 
 		totalExpected += expected
 		filesInfo = append(filesInfo, currentBuildDependencies...)
 		if err != nil {
-			isErrorOccured = true
+			isErrorOccurred = true
 			log.Error(err)
 			log.Info("Downloaded", strconv.Itoa(len(filesInfo)), "artifacts.")
 			continue
 		}
 	}
-	if isErrorOccured {
+	if isErrorOccurred {
 		return len(filesInfo), totalExpected - len(filesInfo), errors.New("Download finished with errors. Please review the logs")
 	}
 	log.Debug("Downloaded", strconv.Itoa(len(filesInfo)), "artifacts.")
