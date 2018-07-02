c5d3f318324df370c4d30c249e39cd0675d0d7ae
eyalbe4 <eyalbenmoshe@jfrog.com>
2018-07-02 22:34:40 +0300

    Support for NuGet build info.

--- jfrog-cli/docs/artifactory/nuget/help.go
+++ jfrog-cli/docs/artifactory/nuget/help.go
@@ -4,8 +4,8 @@ const Description = "Run NuGet."
 
 var Usage = []string{`jfrog rt nuget [command options] <nuget args> <source repository name>`}
 
-const Arguments string = `	nuget args
-		Arguments to run with NuGet command.	
+const Arguments string = `	nuget command
+		The nuget command to run. For example, restore.
 
 	source repository name
 		The source NuGet repository. Can be a local, remote or virtual NuGet repository.`
