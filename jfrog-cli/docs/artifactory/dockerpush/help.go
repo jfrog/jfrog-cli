bfdc4de123d61d6a79b04f87ba65fd7918001a73
yahavi <yahavizhak@gmail.com>
2018-08-07 09:23:41 +0300

    Fixed typo in docker push help

--- jfrog-cli/docs/artifactory/dockerpush/help.go
+++ jfrog-cli/docs/artifactory/dockerpush/help.go
@@ -2,7 +2,7 @@ package dockerpush
 
 const Description = "Docker push."
 
-var Usage = []string{"jfrog docker-push <image tag> <target repo>"}
+var Usage = []string{"jfrog rt docker-push <image tag> <target repo>"}
 
 const Arguments string = `	image tag
 		Docker image tag to push.
