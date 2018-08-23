b4313d5940bc183e89bd2de32fb4742c17b55588
yahavi <yahavizhak@gmail.com>
2018-08-23 11:46:07 +0300

    Appveyor

--- jfrog-cli/utils/lock/lock_test.go
+++ jfrog-cli/utils/lock/lock_test.go
@@ -6,15 +6,59 @@ import (
 	"os"
 	"testing"
 	"time"
+	"math"
 )
 
-func TestLock(t *testing.T) {
+func TestLockMaxInt(t *testing.T) {
 
 	// First creating the first lock object with special pid number that doesn't exists.
-	fmt.Print("The process id is: ", os.Getpid())
-	b, e := isProcessRunning(os.Getpid()*os.Getpid())
-	fmt.Print("Is process exists? ", b, e)
-	getLock(os.Getpid()*os.Getpid(), t)
+	firstLock, _ := getLock(math.MaxInt32, t)
+	// Creating a second lock object with the running PID
+	secondLock, folderName := getLock(os.Getpid(), t)
+
+	// Confirming that only two locks are located in the lock directory
+	files, err := fileutils.ListFiles(folderName, false)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(files) != 2 {
+		t.Error("Expected 2 files but got ", len(files))
+	}
+
+	// Performing lock. This should work since the first lock PID is not running. The Lock() will remove it.
+	err = secondLock.Lock()
+	if err != nil {
+		t.Error(err)
+	}
+	// Unlocking to remove the lock file.
+	err = secondLock.Unlock()
+	if err != nil {
+		t.Error(err)
+	}
+
+	// If timestamp equals, secondLock.Lock() is not expected to delete first lock's file, since os.Getpid() < math.MaxInt32.
+	if firstLock.currentTime == secondLock.currentTime {
+		err = firstLock.Unlock()
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	// Confirming that no locks are located in the lock directory
+	files, err = fileutils.ListFiles(folderName, false)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(files) != 0 {
+		t.Error("Expected 0 files but got", len(files), files)
+	}
+}
+
+func TestLockNegativeInt(t *testing.T) {
+
+	// First creating the first lock object with special pid number that doesn't exists.
+	getLock(-1, t)
 	// Creating a second lock object with the running PID
 	secondLock, folderName := getLock(os.Getpid(), t)
 
