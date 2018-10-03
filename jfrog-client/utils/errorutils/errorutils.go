3d5cfcbbcecdded731dab26e374c5341f3c5ed37
Alexei Vainshtein <alexeiv@jfrog.com>
2018-10-03 09:05:36 +0300

    Removed JFrog Client from the project

--- jfrog-client/utils/errorutils/errorutils.go
+++
@@ -1,8 +0,0 @@
-package errorutils
-
-// Error modes (how should the application behave when the CheckError function is invoked):
-type OnErrorHandler func(error) error
-
-var CheckError = func(err error) error {
-	return err
-}
