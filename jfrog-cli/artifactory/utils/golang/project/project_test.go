bb5893b21fd2f56b891b319cd6d476752cfe0b2a
Alexei Vainshtein <alexeiv@jfrog.com>
2018-12-20 12:07:22 +0200

    Change regex to match quotation marks in mod file

--- jfrog-cli/artifactory/utils/golang/project/project_test.go
+++ jfrog-cli/artifactory/utils/golang/project/project_test.go
@@ -27,6 +27,31 @@ require (
 	}
 }
 
+func TestParseModuleNameWithQuotationMarks(t *testing.T) {
+	content := `
+
+module "github.com/jfrog/go-example"
+
+require (
+        github.com/Sirupsen/logrus v1.0.6
+        golang.org/x/crypto v0.0.0-20180802221240-56440b844dfe // indirect
+        golang.org/x/sys v0.0.0-20180802203216-0ffbfd41fbef // indirect
+        rsc.io/quote v1.5.2
+)
+	`
+
+	expected := "github.com/jfrog/go-example"
+	actual, err := parseModuleName(content)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if actual != expected {
+		t.Errorf("Expected: %s, got: %s.", expected, actual)
+	}
+}
+
 func TestParseModuleNameWithoutSlash(t *testing.T) {
 	content := `
 
