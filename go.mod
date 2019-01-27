d2316ec6fd1245a12c4ea45e9c005e801c80f3b5
Alexei Vainshtein <alexeiv@jfrog.com>
2019-01-27 11:53:50 +0200

    Remove reverting of go.sum for root project

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/jfrog/jfrog-cli-go
 require (
 	github.com/buger/jsonparser v0.0.0-20180910192245-6acdf747ae99
 	github.com/codegangsta/cli v1.20.0
-	github.com/jfrog/gofrog v1.0.2
+	github.com/jfrog/gofrog v1.0.3
 	github.com/jfrog/jfrog-client-go v0.2.1
 	github.com/magiconair/properties v1.8.0
 	github.com/mattn/go-shellwords v1.0.3
@@ -14,4 +14,4 @@ require (
 	gopkg.in/yaml.v2 v2.2.2
 )
 
-replace github.com/jfrog/jfrog-client-go => github.com/jfrog/jfrog-client-go dev
+replace github.com/jfrog/jfrog-client-go => github.com/jfrog/jfrog-client-go v0.2.2-0.20190116093800-a4a3cf86a935
--- jfrog-cli/artifactory/commands/golang/gorecursivepublish.go
+++ jfrog-cli/artifactory/commands/golang/gorecursivepublish.go
@@ -17,31 +17,21 @@ func Execute(targetRepo, goModEditMessage string, serviceManager *artifactory.Ar
 	if err != nil {
 		return err
 	}
-	gci := goConfigInfo{}
+	gmi := goModInfo{}
 	wd, err := os.Getwd()
 	if err != nil {
-		return gci.revert(wd, err)
+		return gmi.revert(wd, err)
 	}
 	if !modFileExists {
-		err = gci.prepareModFile(wd, goModEditMessage)
+		err = gmi.prepareModFile(wd, goModEditMessage)
 		if err != nil {
 			return err
 		}
 	} else {
 		log.Debug("Using existing root mod file.")
-		gci.modFileContent, gci.modFileStat, err = cmd.GetFileDetails("go.mod")
+		gmi.modFileContent, gmi.modFileStat, err = cmd.GetFileDetails("go.mod")
 		if err != nil {
 			return err
-		}
-		gci.shouldRevertSumFile, err = fileutils.IsFileExists("go.sum", false)
-		if err != nil {
-			return err
-		}
-		if gci.shouldRevertSumFile {
-			gci.sumFileContent, gci.sumFileStat, err = cmd.GetFileDetails("go.sum")
-			if err != nil {
-				return err
-			}
 		}
 	}
 
@@ -49,51 +39,32 @@ func Execute(targetRepo, goModEditMessage string, serviceManager *artifactory.Ar
 	if err != nil {
 		if !modFileExists {
 			log.Debug("Graph failed, preparing to run go mod tidy on the root project since got the following error:", err.Error())
-			err = gci.prepareAndRunTidyOnFailedGraph(wd, targetRepo, goModEditMessage, serviceManager)
+			err = gmi.prepareAndRunTidyOnFailedGraph(wd, targetRepo, goModEditMessage, serviceManager)
 			if err != nil {
-				return gci.revert(wd, err)
+				return gmi.revert(wd, err)
 			}
 		} else {
-			return gci.revert(wd, err)
+			return gmi.revert(wd, err)
 		}
 	}
 
 	err = os.Chdir(wd)
 	if err != nil {
-		return gci.revert(wd, err)
+		return gmi.revert(wd, err)
 	}
-	if gci.shouldRevertModFile || gci.shouldRevertSumFile {
-		return gci.revert(wd, nil)
-	}
-	return nil
+	return gmi.revert(wd, nil)
 }
 
-type goConfigInfo struct {
+type goModInfo struct {
 	modFileContent      []byte
 	modFileStat         os.FileInfo
 	shouldRevertModFile bool
-	shouldRevertSumFile bool
-	sumFileContent      []byte
-	sumFileStat         os.FileInfo
 }
 
-func (gci *goConfigInfo) revert(wd string, err error) error {
-	if gci.shouldRevertModFile {
+func (gmi *goModInfo) revert(wd string, err error) error {
+	if gmi.shouldRevertModFile {
 		log.Debug("Reverting to original go.mod of the root project")
-		revertErr := ioutil.WriteFile("go.mod", gci.modFileContent, gci.modFileStat.Mode())
-		if revertErr != nil {
-			if err != nil {
-				log.Error(revertErr)
-				return err
-			} else {
-				return revertErr
-			}
-		}
-	}
-
-	if gci.shouldRevertSumFile {
-		log.Debug("Reverting to original go.sum of the root project")
-		revertErr := ioutil.WriteFile("go.sum", gci.sumFileContent, gci.sumFileStat.Mode())
+		revertErr := ioutil.WriteFile("go.mod", gmi.modFileContent, gmi.modFileStat.Mode())
 		if revertErr != nil {
 			if err != nil {
 				log.Error(revertErr)
@@ -106,7 +77,7 @@ func (gci *goConfigInfo) revert(wd string, err error) error {
 	return nil
 }
 
-func (gci *goConfigInfo) prepareModFile(wd, goModEditMessage string) error {
+func (gmi *goModInfo) prepareModFile(wd, goModEditMessage string) error {
 	err := cmd.RunGoModInit("", goModEditMessage)
 	if err != nil {
 		return err
@@ -116,20 +87,20 @@ func (gci *goConfigInfo) prepareModFile(wd, goModEditMessage string) error {
 		return err
 	}
 	notEmptyModRegex := regExp.GetNotEmptyModRegex()
-	gci.modFileContent, gci.modFileStat, err = cmd.GetFileDetails("go.mod")
+	gmi.modFileContent, gmi.modFileStat, err = cmd.GetFileDetails("go.mod")
 	if err != nil {
 		return err
 	}
 	projectPackage := dependencies.Package{}
-	projectPackage.SetModContent(gci.modFileContent)
+	projectPackage.SetModContent(gmi.modFileContent)
 	packageWithDep := dependencies.PackageWithDeps{Dependency: &projectPackage}
 	if !packageWithDep.PatternMatched(notEmptyModRegex) {
 		log.Debug("Root mod is empty, preparing to run 'go mod tidy'")
 		err = cmd.RunGoModTidy()
 		if err != nil {
-			return gci.revert(wd, err)
+			return gmi.revert(wd, err)
 		}
-		gci.shouldRevertModFile = true
+		gmi.shouldRevertModFile = true
 	} else {
 		log.Debug("Root project mod not empty.")
 	}
@@ -137,13 +108,13 @@ func (gci *goConfigInfo) prepareModFile(wd, goModEditMessage string) error {
 	return nil
 }
 
-func (gci *goConfigInfo) prepareAndRunTidyOnFailedGraph(wd, targetRepo, goModEditMessage string, serviceManager *artifactory.ArtifactoryServicesManager) error {
+func (gmi *goModInfo) prepareAndRunTidyOnFailedGraph(wd, targetRepo, goModEditMessage string, serviceManager *artifactory.ArtifactoryServicesManager) error {
 	// First revert the mod to an empty mod that includes only module name
-	lines := strings.Split(string(gci.modFileContent), "\n")
+	lines := strings.Split(string(gmi.modFileContent), "\n")
 	emptyMod := strings.Join(lines[:3], "\n")
-	gci.modFileContent = []byte(emptyMod)
-	gci.shouldRevertModFile = true
-	err := gci.revert(wd, nil)
+	gmi.modFileContent = []byte(emptyMod)
+	gmi.shouldRevertModFile = true
+	err := gmi.revert(wd, nil)
 	if err != nil {
 		log.Error(err)
 	}
@@ -155,7 +126,7 @@ func (gci *goConfigInfo) prepareAndRunTidyOnFailedGraph(wd, targetRepo, goModEdi
 	// Perform collection again after tidy finished successfully.
 	err = executers.DownloadFromVcsWithPopulation(targetRepo, goModEditMessage, serviceManager)
 	if err != nil {
-		return gci.revert(wd, err)
+		return gmi.revert(wd, err)
 	}
 	return nil
 }
