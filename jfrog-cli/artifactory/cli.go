096300b1f511e41e40a90ad4236e8da50efbcc5a
eyalbe4 <eyalbenmoshe@jfrog.com>
2019-02-19 17:10:57 +0200

    Help fix for --exclude-patterns - only "jfrog rt u" supports regexps.

--- jfrog-cli/artifactory/cli.go
+++ jfrog-cli/artifactory/cli.go
@@ -564,10 +564,6 @@ func getUploadFlags() []cli.Flag {
 			Usage: "[Default: false] Set to true to use a regular expression instead of wildcards expression to collect files to upload.` `",
 		},
 		cli.StringFlag{
-			Name:  "exclude-patterns",
-			Usage: "[Optional] Semicolon-separated list of exclude patterns. Exclude patterns may contain the * and the ? wildcards or a regex pattern, according to the value of the 'regexp' option.` `",
-		},
-		cli.StringFlag{
 			Name:  "retries",
 			Usage: "[Default: " + strconv.Itoa(cliutils.Retries) + "] Number of upload retries.` `",
 		},
@@ -587,6 +583,7 @@ func getUploadFlags() []cli.Flag {
 			Name:  "include-dirs",
 			Usage: "[Default: false] Set to true if you'd like to also apply the source path pattern for directories and not just for files.` `",
 		},
+		getUploadExcludePatternsFlag(),
 		getFailNoOpFlag(),
 		getThreadsFlag(),
 	}...)
@@ -687,7 +684,14 @@ func getFailNoOpFlag() cli.Flag {
 func getExcludePatternsFlag() cli.Flag {
 	return cli.StringFlag{
 		Name:  "exclude-patterns",
-		Usage: "[Optional] Semicolon-separated list of exclude patterns. Exclude patterns may contain the * and the ? wildcards.` `",
+		Usage: "[Optional] Semicolon-separated list of exclude patterns. Exclude patterns may contain the * and the ? wildcards. Unlike the Source path, it must not include the repository name at the beginning of the path.` `",
+	}
+}
+
+func getUploadExcludePatternsFlag() cli.Flag {
+	return cli.StringFlag{
+		Name:  "exclude-patterns",
+		Usage: "[Optional] Semicolon-separated list of exclude patterns. Exclude patterns may contain the * and the ? wildcards or a regex pattern, according to the value of the 'regexp' option.` `",
 	}
 }
 
@@ -987,7 +991,7 @@ func getBuildAddDependenciesFlags() []cli.Flag {
 			Name:  "dry-run",
 			Usage: "[Default: false] Set to true to only get a summery of the dependencies that will be added to the build info.` `",
 		},
-		getExcludePatternsFlag(),
+		getUploadExcludePatternsFlag(),
 	}...)
 }
 
