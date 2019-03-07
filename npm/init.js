8136d43cfa6201920af2d1e2eb601e48c6551dbb
Roland Broekema <broekema41@gmail.com>
2019-03-07 11:19:08 +0100

    https_proxy fix for download behind a web proxy on CI machines (#337)
    * https_proxy support fix for download behind a web proxy on CI machines
    
    * simpefy the code after review comments
    
    * bugfix for protocol error during test on remote server
    
    * updates to the code after second review
    
    * proxydownload now connects over a connect socket just like curl would do.

--- npm/init.js
+++ npm/init.js
@@ -1,6 +1,8 @@
 validateNpmVersion();
 
-var https = require('follow-redirects').https;
+var https = require('https');
+var http = require('http');
+var url = require('url');
 var fs = require('fs');
 var packageJson = require('./package.json');
 var fileName = getFileName();
@@ -12,19 +14,58 @@ downloadCli();
 
 function validateNpmVersion() {
     if (!isValidNpmVersion()) {
-        throw new Error("JFrog CLI can be installed using npm version 5.0.0 or above.")
+        throw new Error("JFrog CLI can be installed using npm version 5.0.0 or above.");
     }
+}
+
+function downloadWithProxy(myUrl) {
+    var proxyparts = url.parse(process.env.https_proxy);
+    var myUrlParts = url.parse(myUrl);
+
+    http.request({
+        host: proxyparts.hostname,
+        port: proxyparts.port,
+        method: 'CONNECT',
+        path: myUrlParts.hostname + ':443'
+    }).on('connect', function(res, socket, head) {
+        https.get({
+            host: myUrlParts.hostname,
+            socket: socket,
+            path: myUrlParts.path,
+            agent: false
+        }, function(res) {
+            if (res.statusCode == 301 || res.statusCode == 302) {
+                downloadWithProxy(res.headers.location);
+            } else if (res.statusCode == 200) {
+                writeToFile(res);
+            } else {
+                console.log('Unexpected status code ' + res.statusCode + ' during JFrog CLI download');
+            }
+        }).on('error', function (err) {console.error(err);});
+    }).end();
+}
+
+function download(url) {
+    https.get(url, function(res) {
+        if (res.statusCode == 301 || res.statusCode == 302) {
+            download(res.headers.location);
+        } else if (res.statusCode == 200) {
+            writeToFile(res);
+        } else {
+            console.log('Unexpected status code ' + res.statusCode + ' during JFrog CLI download');
+        }
+    }).on('error', function (err) {console.error(err);});
 }
 
 function downloadCli() {
     console.log("Downloading JFrog CLI " + version );
-    https.get({
-        hostname: 'api.bintray.com',
-        port: 443,
-        path: '/content/jfrog/jfrog-cli-go/' + version + '/' + btPackage + '/' + fileName + '?bt_package=' + btPackage,
-        agent: false,
-        followAllRedirects: true
-    }, writeToFile).on('error', function (err) {console.error(err);});
+    var startUrl = 'https://api.bintray.com/content/jfrog/jfrog-cli-go/' + version + '/' + btPackage + '/' + fileName + '?bt_package=' + btPackage;
+    // We detect outbount proxy by looking at the environment variable
+    if (process.env.https_proxy && process.env.https_proxy.length > 0) {
+        downloadWithProxy(startUrl);
+    } else {
+        download(startUrl);
+    }
 }
 
 function isValidNpmVersion() {
@@ -32,7 +73,7 @@ function isValidNpmVersion() {
     var npmVersionCmdOut = child_process.execSync("npm version -json");
     var npmVersion = JSON.parse(npmVersionCmdOut).npm;
     // Supported since version 5.0.0
-    return parseInt(npmVersion.charAt(0)) > 4
+    return parseInt(npmVersion.charAt(0)) > 4;
 }
 
 function writeToFile(response) {
@@ -42,7 +83,7 @@ function writeToFile(response) {
     }).on('end', function () {
         file.end();
         if (!process.platform.startsWith("win")) {
-            fs.chmodSync(filePath, 0555)
+            fs.chmodSync(filePath, 0555);
         }
     }).on('error', function (err) {
         console.error(err);
@@ -52,21 +93,21 @@ function writeToFile(response) {
 function getArchitecture() {
     var platform = process.platform;
     if (platform.startsWith("win")) {
-        return "windows-amd64"
+        return "windows-amd64";
     }
     if (platform.includes("darwin")) {
-        return "mac-386"
+        return "mac-386";
     }
     if (process.arch.includes("64")) {
-        return "linux-amd64"
+        return "linux-amd64";
     }
-    return "linux-386"
+    return "linux-386";
 }
 
 function getFileName() {
     var excecutable = "jfrog";
     if (process.platform.startsWith("win")) {
-        excecutable += ".exe"
+        excecutable += ".exe";
     }
-    return excecutable
+    return excecutable;
 }
