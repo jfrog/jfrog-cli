ee65137beb8645e78667bc394b273c3768a69028
AlexeiVainshtein <alexeiv@jfrog.com>
2019-03-31 14:22:44 +0300

    Nuget disabled project will not appear in build info

--- jfrog-cli/artifactory/utils/nuget/solution/solution.go
+++ jfrog-cli/artifactory/utils/nuget/solution/solution.go
@@ -4,9 +4,13 @@ import (
 	"encoding/json"
 	"github.com/jfrog/jfrog-cli-go/jfrog-cli/artifactory/utils/nuget/solution/project"
 	"github.com/jfrog/jfrog-client-go/artifactory/buildinfo"
-	"github.com/jfrog/jfrog-client-go/utils/errorutils"
-	"os"
+	"github.com/jfrog/jfrog-client-go/utils"
+	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
+	"github.com/jfrog/jfrog-client-go/utils/log"
+	"github.com/pkg/errors"
+	"io/ioutil"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
@@ -16,14 +20,15 @@ type Solution interface {
 	GetProjects() []project.Project
 }
 
-func Load(solutionPath string) (Solution, error) {
-	solution := &solution{path: solutionPath}
+func Load(solutionPath, slnFile string) (Solution, error) {
+	solution := &solution{path: solutionPath, slnFile: slnFile}
 	err := solution.loadProjects()
 	return solution, err
 }
 
 type solution struct {
 	path     string
+	slnFile  string
 	projects []project.Project
 }
 
@@ -62,27 +67,88 @@ func (solution *solution) GetProjects() []project.Project {
 }
 
 func (solution *solution) loadProjects() error {
-	return filepath.Walk(solution.path, func(path string, f os.FileInfo, err error) error {
+	regExp, err := utils.GetRegExp(`Project\("(.*)\nEndProject`)
+	if err != nil {
+		return err
+	}
+
+	allProjects, err := solution.getProjectsFromSlns(regExp)
+	if err != nil {
+		return err
+	}
+
+	for _, projectLine := range allProjects {
+		projectName, csprojPath, err := parseProject(projectLine, solution.path)
 		if err != nil {
-			return errorutils.CheckError(err)
+			log.Error(err)
+			continue
 		}
-		if f.IsDir() {
-			return nil
+		proj, err := project.Load(projectName, filepath.Dir(csprojPath), csprojPath)
+		if err != nil {
+			log.Error(err)
+			continue
 		}
-		if filepath.Ext(f.Name()) == ".csproj" {
-			projectName := strings.TrimSuffix(f.Name(), ".csproj")
-			csprojPath, err := filepath.Rel(solution.path, path)
+		if proj.Extractor() != nil {
+			solution.projects = append(solution.projects, proj)
+		}
+	}
+	return nil
+}
+
+// Finds all the project section within the sln files. If sln file is not provided,
+// searching the entire directory for all sln files.
+// Returns slice of all the projects.
+func (solution *solution) getProjectsFromSlns(regExp *regexp.Regexp) ([]string, error) {
+	var allProjects []string
+	if solution.slnFile == "" {
+		slnFiles, err := fileutils.ListFilesWithSpecificExtension(solution.path, ".sln")
+		if err != nil {
+			return nil, err
+		}
+		for _, slnFile := range slnFiles {
+			projects, err := parseSlnFile(slnFile, regExp)
 			if err != nil {
-				return errorutils.CheckError(err)
+				return nil, err
 			}
-			proj, err := project.Load(projectName, filepath.Dir(csprojPath), csprojPath)
-			if err != nil {
-				return err
-			}
-			if proj.Extractor() != nil {
-				solution.projects = append(solution.projects, proj)
-			}
+			allProjects = append(allProjects, projects...)
 		}
-		return nil
-	})
+	} else {
+		projects, err := parseSlnFile(filepath.Join(solution.path, solution.slnFile), regExp)
+		if err != nil {
+			return nil, err
+		}
+		allProjects = append(allProjects, projects...)
+	}
+	return allProjects, nil
+}
+
+// Parsing the project line for the project name and path information.
+// Returns the name and path to csproj
+func parseProject(projectLine, path string) (projectName, csprojPath string, err error) {
+	parsedLine := strings.Split(projectLine, "=")
+	if len(parsedLine) <= 1 {
+		return "", "", errors.New("Wrong number of arguments for project line:" + projectLine)
+	}
+
+	projectInfo := strings.Split(parsedLine[1], ",")
+	if len(projectInfo) <= 2 {
+		return "", "", errors.New("Wrong number of arguments for project information: " + parsedLine[1])
+	}
+	projectName = removeApostrophes(projectInfo[0])
+	csprojPath = filepath.Join(path, removeApostrophes(projectInfo[1]))
+	return
+}
+
+// Parse the sln file and returns all the founded lines by the regex
+func parseSlnFile(slnFile string, regExp *regexp.Regexp) ([]string, error) {
+	content, err := ioutil.ReadFile(slnFile)
+	if err != nil {
+		return nil, err
+	}
+	projects := regExp.FindAllString(string(content), -1)
+	return projects, nil
+}
+
+func removeApostrophes(value string) string {
+	return strings.Trim(strings.TrimSpace(value), "\"")
 }
--- jfrog-cli/artifactory/utils/nuget/solution/solution_test.go
+++ jfrog-cli/artifactory/utils/nuget/solution/solution_test.go
@@ -2,13 +2,17 @@ package solution
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/jfrog/jfrog-client-go/artifactory/buildinfo"
+	"github.com/jfrog/jfrog-client-go/utils"
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 )
 
 func TestEmptySolution(t *testing.T) {
-	solution, err := Load(".")
+	solution, err := Load(".", "")
 	if err != nil {
 		t.Error(err)
 	}
@@ -22,5 +26,114 @@ func TestEmptySolution(t *testing.T) {
 		expectedString, _ := json.Marshal(expected)
 		buildInfoString, _ := json.Marshal(buildInfo)
 		t.Errorf("Expecting: \n%s \nGot: \n%s", expectedString, buildInfoString)
+	}
+}
+
+func TestParseSln(t *testing.T) {
+	regExp, err := utils.GetRegExp(`Project\("(.*)\nEndProject`)
+	if err != nil {
+		t.Error(err)
+	}
+	pwd, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+
+	testsDataDir := filepath.Join(pwd, "testsdata")
+
+	tests := []struct {
+		name     string
+		slnPath  string
+		expected []string
+	}{
+		{"oneproject", filepath.Join(testsDataDir, "oneproject.sln"), []string{`Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "packagesconfig", "packagesconfig.csproj", "{D1FFA0DC-0ACC-4108-ADC1-2A71122C09AF}"
+EndProject`}},
+		{"multiProjects", filepath.Join(testsDataDir, "multiprojects.sln"), []string{`Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "packagesconfigmulti", "packagesconfig.csproj", "{D1FFA0DC-0ACC-4108-ADC1-2A71122C09AF}"
+EndProject`, `Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "packagesconfiganothermulti", "test\packagesconfig.csproj", "{D1FFA0DC-0ACC-4108-ADC1-2A71122C09AF}"
+EndProject`}},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result, err := parseSlnFile(test.slnPath, regExp)
+			if err != nil {
+				t.Error(err)
+			}
+			if !reflect.DeepEqual(test.expected, result) {
+				t.Error(fmt.Sprintf("Expected %s, got %s", test.expected, result))
+			}
+		})
+	}
+}
+
+func TestParseProject(t *testing.T) {
+
+	tests := []struct {
+		name                string
+		projectLine         string
+		expectedCsprojPath  string
+		expectedProjectName string
+	}{
+		{"packagename", `Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "packagename", "packagesconfig.csproj", "{D1FFA0DC-0ACC-4108-ADC1-2A71122C09AF}"
+EndProject`, "jfrog/path/test/packagesconfig.csproj", "packagename"},
+		{"withpath", `Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "packagename", "packagesconfig/packagesconfig.csproj", "{D1FFA0DC-0ACC-4108-ADC1-2A71122C09AF}"
+EndProject`, "jfrog/path/test/packagesconfig/packagesconfig.csproj", "packagename"},
+		{"sameprojectname", `Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "packagesconfig", "packagesconfig/packagesconfig.csproj", "{D1FFA0DC-0ACC-4108-ADC1-2A71122C09AF}"
+EndProject`, "jfrog/path/test/packagesconfig/packagesconfig.csproj", "packagesconfig"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			csprojPath, projectName, err := parseProject(test.projectLine, "jfrog/path/test")
+			if err != nil {
+				t.Error(err)
+			}
+			if csprojPath != test.expectedCsprojPath {
+				t.Error(fmt.Sprintf("Expected %s, got %s", test.expectedCsprojPath, csprojPath))
+			}
+			if projectName != test.expectedProjectName {
+				t.Error(fmt.Sprintf("Expected %s, got %s", test.expectedProjectName, projectName))
+			}
+		})
+	}
+}
+
+func TestGetProjectsFromSlns(t *testing.T) {
+	regExp, err := utils.GetRegExp(`Project\("(.*)\nEndProject`)
+	if err != nil {
+		t.Error(err)
+	}
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+
+	testsDataDir := filepath.Join(pwd, "testsdata")
+	tests := []struct {
+		name             string
+		solution         solution
+		expectedProjects []string
+	}{
+		{"withoutSlnFile", solution{path: testsDataDir, slnFile: "", projects: nil}, []string{`Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "packagesconfigmulti", "packagesconfig.csproj", "{D1FFA0DC-0ACC-4108-ADC1-2A71122C09AF}"
+EndProject`, `Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "packagesconfiganothermulti", "test\packagesconfig.csproj", "{D1FFA0DC-0ACC-4108-ADC1-2A71122C09AF}"
+EndProject`, `Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "packagesconfig", "packagesconfig.csproj", "{D1FFA0DC-0ACC-4108-ADC1-2A71122C09AF}"
+EndProject`},
+		},
+		{"withSlnFile", solution{path: testsDataDir, slnFile: "oneproject.sln", projects: nil}, []string{`Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "packagesconfig", "packagesconfig.csproj", "{D1FFA0DC-0ACC-4108-ADC1-2A71122C09AF}"
+EndProject`},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result, err := test.solution.getProjectsFromSlns(regExp)
+			if err != nil {
+				t.Error(err)
+			}
+
+			if !reflect.DeepEqual(test.expectedProjects, result) {
+				t.Error(fmt.Sprintf("Expected %s, got %s", test.expectedProjects, result))
+			}
+		})
 	}
 }
