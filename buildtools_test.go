36ce87c85e51bb551d36887fe3dc30f8ae1b5716
AlexeiVainshtein <alexeiv@jfrog.com>
2019-06-18 10:45:51 +0300

    Build name and number are not set correctly when using gradle with uses plugin

--- buildtools_test.go
+++ buildtools_test.go
@@ -126,14 +126,38 @@ func runAndValidateMaven(pomPath, configFilePath string, t *testing.T) {
 func TestGradleBuildWithServerID(t *testing.T) {
 	initBuildToolsTest(t)
 
-	buildGradlePath := createGradleProject(t)
+	buildGradlePath := createGradleProject(t, "gradleproject")
 	configFilePath := filepath.Join(filepath.FromSlash(tests.GetTestResourcesPath()), "buildspecs", tests.GradleServerIDConfig)
 	configFilePath, err := tests.ReplaceTemplateVariables(configFilePath, "")
 	if err != nil {
 		t.Error(err)
 	}
+	buildName := "gradle-cli"
+	buildNumber := "1"
+	runAndValidateGradle(buildGradlePath, configFilePath, t, &utils.BuildConfiguration{BuildName: buildName, BuildNumber: buildNumber})
+	artifactoryCli.Exec("bp", buildName, buildNumber)
+	buildInfo := inttestutils.GetBuildInfo(artifactoryDetails.Url, buildName, buildNumber, t, artHttpDetails)
+	validateBuildInfo(buildInfo, t, 0, 1, ":minimal-example:1.0")
 
-	runAndValidateGradle(buildGradlePath, configFilePath, t)
+	cleanBuildToolsTest()
+}
+
+func TestGradleBuildWithServerIDWithUsesPlugin(t *testing.T) {
+	initBuildToolsTest(t)
+
+	buildGradlePath := createGradleProject(t, "projectwithplugin")
+	configFilePath := filepath.Join(filepath.FromSlash(tests.GetTestResourcesPath()), "buildspecs", tests.GradleServerIDUsesPluginConfig)
+	configFilePath, err := tests.ReplaceTemplateVariables(configFilePath, "")
+	if err != nil {
+		t.Error(err)
+	}
+	buildName := "gradle-cli"
+	buildNumber := "1"
+	runAndValidateGradle(buildGradlePath, configFilePath, t, &utils.BuildConfiguration{BuildName: buildName, BuildNumber: buildNumber})
+
+	artifactoryCli.Exec("bp", buildName, buildNumber)
+	buildInfo := inttestutils.GetBuildInfo(artifactoryDetails.Url, buildName, buildNumber, t, artHttpDetails)
+	validateBuildInfo(buildInfo, t, 0, 1, ":minimal-example:1.0")
 	cleanBuildToolsTest()
 }
 
@@ -144,14 +168,19 @@ func TestGradleBuildWithCredentials(t *testing.T) {
 
 	initBuildToolsTest(t)
 
-	buildGradlePath := createGradleProject(t)
+	buildName := "gradle-cli"
+	buildNumber := "1"
+	buildGradlePath := createGradleProject(t, "gradleproject")
 	srcConfigTemplate := filepath.Join(filepath.FromSlash(tests.GetTestResourcesPath()), "buildspecs", tests.GradleUseramePasswordTemplate)
 	configFilePath, err := tests.ReplaceTemplateVariables(srcConfigTemplate, "")
 	if err != nil {
 		t.Error(err)
 	}
 
-	runAndValidateGradle(buildGradlePath, configFilePath, t)
+	runAndValidateGradle(buildGradlePath, configFilePath, t, &utils.BuildConfiguration{BuildName: buildName, BuildNumber: buildNumber})
+	artifactoryCli.Exec("bp", buildName, buildNumber)
+	buildInfo := inttestutils.GetBuildInfo(artifactoryDetails.Url, buildName, buildNumber, t, artHttpDetails)
+	validateBuildInfo(buildInfo, t, 0, 1, ":minimal-example:1.0")
 	cleanBuildToolsTest()
 }
 
@@ -1120,8 +1149,7 @@ func initNpmTest(t *testing.T) (npmProjectPath, npmScopedProjectPath, npmNpmrcPr
 	return
 }
 
-func runAndValidateGradle(buildGradlePath, configFilePath string, t *testing.T) {
-	buildConfig := &utils.BuildConfiguration{}
+func runAndValidateGradle(buildGradlePath, configFilePath string, t *testing.T, buildConfig *utils.BuildConfiguration) {
 	gradleCmd := gradle.NewGradleCommand().SetTasks("clean artifactoryPublish -b " + buildGradlePath).SetConfigPath(configFilePath).SetConfiguration(buildConfig)
 	err := gradleCmd.Run()
 	if err != nil {
@@ -1133,16 +1161,17 @@ func runAndValidateGradle(buildGradlePath, configFilePath string, t *testing.T) 
 	}
 
 	isExistInArtifactory(tests.GetGradleDeployedArtifacts(), searchSpec, t)
+	isExistInArtifactoryByProps(tests.GetGradleDeployedArtifacts(), tests.Repo1+"/*", "build.name="+buildConfig.BuildName+";build.number="+buildConfig.BuildNumber, t)
 }
 
-func createGradleProject(t *testing.T) string {
-	srcBuildFile := filepath.Join(filepath.FromSlash(tests.GetTestResourcesPath()), "gradleproject", "build.gradle")
+func createGradleProject(t *testing.T, projectName string) string {
+	srcBuildFile := filepath.Join(filepath.FromSlash(tests.GetTestResourcesPath()), "gradle", projectName, "build.gradle")
 	buildGradlePath, err := tests.ReplaceTemplateVariables(srcBuildFile, "")
 	if err != nil {
 		t.Error(err)
 	}
 
-	srcSettingsFile := filepath.Join(filepath.FromSlash(tests.GetTestResourcesPath()), "gradleproject", "settings.gradle")
+	srcSettingsFile := filepath.Join(filepath.FromSlash(tests.GetTestResourcesPath()), "gradle", projectName, "settings.gradle")
 	_, err = tests.ReplaceTemplateVariables(srcSettingsFile, "")
 	if err != nil {
 		t.Error(err)
