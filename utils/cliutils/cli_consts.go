0a2645e167d630b447ec929a60bd78e149f8294f
eyalbe4 <eyalbenmoshe@jfrog.com>
2019-09-26 15:08:34 +0300

    Update version to 1.29.1

--- utils/cliutils/cli_consts.go
+++ utils/cliutils/cli_consts.go
@@ -2,7 +2,7 @@ package cliutils
 
 const (
 	// General CLI constants
-	CliVersion           = "1.29.0"
+	CliVersion           = "1.29.1"
 	ClientAgent          = "jfrog-cli-go"
 	OnErrorPanic OnError = "panic"
 
