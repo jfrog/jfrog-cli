a61d559050f56f2f32b9ee79e8234a2af16fc16e
Or Gabay <ortil1@gmail.com>
2019-10-10 18:00:42 +0300

    Add '.gitignore' file to gp (#466)
    * Add '.gitignore' file to gp
    
    * Ignore files according to 'gitignore' pattern
    
    Fix Pr comment
    
    * Update imports
    
    * Fix PR comments
    
    * check if '.gitignore' exist before gp
    
    * Fix go.mod

--- artifactory/utils/golang/project/archive.go
+++ artifactory/utils/golang/project/archive.go
@@ -3,20 +3,24 @@ package project
 import (
 	"archive/zip"
 	"fmt"
-	"github.com/jfrog/jfrog-cli-go/utils/ioutils"
-	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
-	"github.com/jfrog/jfrog-client-go/utils/log"
 	"io"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
+
+	"github.com/denormal/go-gitignore"
+	"github.com/jfrog/jfrog-cli-go/utils/ioutils"
+	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
+	"github.com/jfrog/jfrog-client-go/utils/log"
 )
 
 // Archive project files according to the go project standard
 func archiveProject(writer io.Writer, sourcePath, module, version string, excludePathsRegExp *regexp.Regexp) error {
 	zipWriter := zip.NewWriter(writer)
 	defer zipWriter.Close()
+
+	ignore, gitIgnoreErr := gitignore.NewFromFile(sourcePath + "/.gitignore")
 
 	return filepath.Walk(sourcePath, func(path string, info os.FileInfo, err error) error {
 		if err != nil || info == nil || info.IsDir() || fileutils.IsPathSymlink(path) {
@@ -27,6 +31,12 @@ func archiveProject(writer io.Writer, sourcePath, module, version string, exclud
 			log.Debug(fmt.Sprintf("Excluding path '%s' from zip archive.", path))
 			return nil
 		}
+
+		if gitIgnoreErr == nil && isPathIgnoredByGit(path, ignore) {
+			log.Debug(fmt.Sprintf("Excluding path '%s' from zip archive (caused by gitignore).", path))
+			return nil
+		}
+
 		fileName := getFileName(sourcePath, path, module, version)
 		file, err := os.Open(path)
 		if err != nil {
@@ -52,4 +62,17 @@ func getFileName(sourcePath, filePath, moduleName, version string) string {
 	moduleID := fmt.Sprintf("%s@%s", moduleName, version)
 
 	return filepath.Join(moduleID, filename)
+}
+
+func isPathIgnoredByGit(path string, ignore gitignore.GitIgnore) bool {
+	if ignore == nil {
+		return false
+	}
+
+	match := ignore.Match(path)
+	if match != nil {
+		return match.Ignore()
+	}
+
+	return false
 }
