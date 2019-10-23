4379c7c60ceb272d98c7c3e41f9f7cc7320e24eb
Bar Belity <barb@jfrog.com>
2019-10-23 19:26:54 +0300

    Pip Install: Fix command description (#473)

--- docs/artifactory/pipinstall/help.go
+++ docs/artifactory/pipinstall/help.go
@@ -2,7 +2,7 @@ package pipinstall
 
 const Description = "Run pip install."
 
-var Usage = []string{`jfrog rt pipi [command options] <pip command>`}
+var Usage = []string{`jfrog rt pipi <pip command>`}
 
 const Arguments string = `	pip command
 		Arguments and options for the pip command.`
