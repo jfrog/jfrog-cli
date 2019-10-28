be54444a9cc8b6b1b057ada61f0d2c24f2542fea
Or Gabay <orgab@jfrog.com>
2019-10-28 10:57:43 +0200

    Split build tools tests

---
+++ maven_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/jfrog/jfrog-cli-go/artifactory/commands/mvn"
+	"github.com/jfrog/jfrog-cli-go/artifactory/utils"
+	"github.com/jfrog/jfrog-cli-go/utils/tests"
+)
+
+func TestMavenBuildWithServerID(t *testing.T) {
+	initBuildToolsTest(t)
+
+	pomPath := createMavenProject(t)
+	configFilePath := filepath.Join(filepath.FromSlash(tests.GetTestResourcesPath()), "buildspecs", tests.MavenServerIDConfig)
+	configFilePath, err := tests.ReplaceTemplateVariables(configFilePath, "")
+	if err != nil {
+		t.Error(err)
+	}
+	runAndValidateMaven(pomPath, configFilePath, t)
+	cleanBuildToolsTest()
+}
+
+func TestMavenBuildWithCredentials(t *testing.T) {
+	if *tests.RtUser == "" || *tests.RtPassword == "" {
+		t.SkipNow()
+	}
+
+	initBuildToolsTest(t)
+
+	pomPath := createMavenProject(t)
+	srcConfigTemplate := filepath.Join(filepath.FromSlash(tests.GetTestResourcesPath()), "buildspecs", tests.MavenUsernamePasswordTemplate)
+	configFilePath, err := tests.ReplaceTemplateVariables(srcConfigTemplate, "")
+	if err != nil {
+		t.Error(err)
+	}
+
+	runAndValidateMaven(pomPath, configFilePath, t)
+	cleanBuildToolsTest()
+}
+
+func runAndValidateMaven(pomPath, configFilePath string, t *testing.T) {
+	buildConfig := &utils.BuildConfiguration{}
+	mvnCmd := mvn.NewMvnCommand().SetConfiguration(buildConfig).SetGoals("clean install -f" + pomPath).SetConfigPath(configFilePath)
+	err := mvnCmd.Run()
+	if err != nil {
+		t.Error(err)
+	}
+	searchSpec, err := tests.CreateSpec(tests.SearchAllRepo1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	isExistInArtifactory(tests.GetMavenDeployedArtifacts(), searchSpec, t)
+}
+
+func createMavenProject(t *testing.T) string {
+	srcPomFile := filepath.Join(filepath.FromSlash(tests.GetTestResourcesPath()), "mavenproject", "pom.xml")
+	pomPath, err := tests.ReplaceTemplateVariables(srcPomFile, "")
+	if err != nil {
+		t.Error(err)
+	}
+	return pomPath
+}
