5bd8d916d8e471145c1e5b145204d596941fadd4
Or Gabay <orgab@jfrog.com>
2019-11-14 14:39:00 +0200

    Native NuGet

---
+++ artifactory/commands/nuget/nugetconfig.go
@@ -0,0 +1,60 @@
+package nuget
+
+import (
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/jfrog/jfrog-cli-go/artifactory/utils"
+	"github.com/jfrog/jfrog-cli-go/artifactory/utils/prompt"
+	"github.com/jfrog/jfrog-client-go/utils/errorutils"
+	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
+	"github.com/jfrog/jfrog-client-go/utils/log"
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
+)
+
+func CreateBuildConfig(global bool) error {
+	projectDir, err := utils.GetProjectDir(global)
+	if err != nil {
+		return err
+	}
+	err = fileutils.CreateDirIfNotExist(projectDir)
+	if err != nil {
+		return err
+	}
+
+	configFilePath := filepath.Join(projectDir, "nuget.yaml")
+	if err := prompt.VerifyConfigFile(configFilePath); err != nil {
+		return err
+	}
+
+	var vConfig *viper.Viper
+	configResult := &NugetBuildConfig{}
+	configResult.Version = prompt.BUILD_CONF_VERSION
+	configResult.ConfigType = utils.Nuget.String()
+	configResult.Resolver.ServerId, vConfig, err = prompt.ReadServerId()
+	if err != nil {
+		return err
+	}
+	configResult.Resolver.Repo, err = prompt.ReadRepo("Set repository for dependencies resolution (press Tab for options): ", vConfig, utils.REMOTE, utils.VIRTUAL)
+	if err != nil {
+		return err
+	}
+	resBytes, err := yaml.Marshal(&configResult)
+	if err != nil {
+		return errorutils.CheckError(err)
+	}
+	err = ioutil.WriteFile(configFilePath, resBytes, 0644)
+	if err != nil {
+		return errorutils.CheckError(err)
+	}
+
+	log.Info("Nuget install config successfully created.")
+	return nil
+
+}
+
+type NugetBuildConfig struct {
+	prompt.CommonConfig `yaml:"common,inline"`
+	Resolver            utils.Repository `yaml:"resolver,omitempty"`
+}
