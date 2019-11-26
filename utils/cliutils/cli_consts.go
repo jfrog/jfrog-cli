9839215c5023dbacf9e1a9fab55fb1615c52da49
barbelity <barb@jfrog.com>
2019-11-26 15:30:29 +0200

    Promoted version to 1.31.0

--- utils/cliutils/cli_consts.go
+++ utils/cliutils/cli_consts.go
@@ -2,7 +2,7 @@ package cliutils
 
 const (
 	// General CLI constants
-	CliVersion           = "1.30.4"
+	CliVersion           = "1.31.0"
 	ClientAgent          = "jfrog-cli-go"
 	OnErrorPanic OnError = "panic"
 
