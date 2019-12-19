4d7b4ac7d179372439dd85538e660d874b57c062
Liron Shalom <lirons@jfrog.com>
2019-12-19 13:48:56 +0200

    Check if sync-delete exists after downloading files.

--- artifactory/commands/generic/download.go
+++ artifactory/commands/generic/download.go
@@ -53,18 +53,9 @@ func (dc *DownloadCommand) CommandName() string {
 }
 
 func (dc *DownloadCommand) Run() error {
-	if dc.SyncDeletesPath() != "" {
-		absSyncDeletesPath, err := filepath.Abs(dc.SyncDeletesPath())
-		if err != nil {
-			return nil
-		}
-		if _, err = os.Stat(absSyncDeletesPath); os.IsNotExist(err) {
-			return errors.New("Sync-deletes path does not exists.")
-		}
-		if !dc.Quiet() && !cliutils.InteractiveConfirm("Sync-deletes may delete some files in your local file system. Are you sure you want to continue?\n"+
-			"You can avoid this confirmation message by adding --quiet to the command.") {
-			return nil
-		}
+	if dc.SyncDeletesPath() != "" && !dc.Quiet() && !cliutils.InteractiveConfirm("Sync-deletes may delete some files in your local file system. Are you sure you want to continue?\n"+
+		"You can avoid this confirmation message by adding --quiet to the command.") {
+		return nil
 	}
 	// Initialize Progress bar, set logger to a log file
 	var err error
@@ -121,10 +112,18 @@ func (dc *DownloadCommand) Run() error {
 		dc.result.SetFailCount(0)
 		return err
 	} else if dc.SyncDeletesPath() != "" {
-		walkFn := createSyncDeletesWalkFunction(filesInfo)
-		err = fileutils.Walk(dc.SyncDeletesPath(), walkFn, false)
+		absSyncDeletesPath, err := filepath.Abs(dc.SyncDeletesPath())
 		if err != nil {
-			return err
+			return nil
+		}
+		if _, err = os.Stat(absSyncDeletesPath); err == nil {
+			walkFn := createSyncDeletesWalkFunction(filesInfo)
+			err = fileutils.Walk(dc.SyncDeletesPath(), walkFn, false)
+			if err != nil {
+				return err
+			}
+		} else if os.IsNotExist(err) {
+			log.Info("Sync-deletes path does not exists.")
 		}
 	}
 	log.Debug("Downloaded", strconv.Itoa(len(filesInfo)), "artifacts.")
