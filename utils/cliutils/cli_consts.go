a238e9d7a61318dfbcfc29924a55e0b6009747ea
Eyal Ben Moshe <eyalb@jfrog.com>
2020-05-25 23:26:10 +0300

    Fix version

--- utils/cliutils/cli_consts.go
+++ utils/cliutils/cli_consts.go
@@ -2,7 +2,7 @@ package cliutils
 
 const (
 	// General CLI constants
-	CliVersion           = "1.35.5"
+	CliVersion           = "1.36.0"
 	ClientAgent          = "jfrog-cli-go"
 	OnErrorPanic OnError = "panic"
 
