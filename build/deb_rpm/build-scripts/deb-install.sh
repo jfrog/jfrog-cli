e580034a75b338856a128dece7173a8aac1eb147
Eyal Ben Moshe <eyalb@jfrog.com>
2020-07-16 09:50:52 +0300

    rpm and deb installers improvements

--- build/deb_rpm/build-scripts/deb-install.sh
+++ build/deb_rpm/build-scripts/deb-install.sh
@@ -1,4 +1,4 @@
 wget -qO - https://releases.jfrog.io/artifactory/api/gpg/key/public | apt-key add -;
 echo "deb https://releases.jfrog.io/artifactory/jfrog-debs xenial contrib" | sudo tee -a /etc/apt/sources.list;
 apt update;
-sudo apt-get install jfrog-cli
+sudo apt install -y jfrog-cli
