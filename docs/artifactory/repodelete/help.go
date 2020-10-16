bb00bcae7aa97b44b21839e31980acb7a2afb1c2
yahavi <yahavi@jfrog.com>
2020-10-16 13:27:42 +0300

    Code review changes

--- docs/artifactory/repodelete/help.go
+++ docs/artifactory/repodelete/help.go
@@ -1,8 +1,8 @@
 package repodelete
 
-const Description = "Permanently delete repositories with all of its content from Artifactory."
+const Description = "Permanently delete repositories with all of their content from Artifactory."
 
 var Usage = []string{`jfrog rt rdel <repository pattern>`}
 
 const Arguments string = `	repository pattern
-		Specifies the repository that should be removed. You can use wildcards to specify multiple repositories.`
+		Specifies the repositories that should be removed. You can use wildcards to specify multiple repositories.`
