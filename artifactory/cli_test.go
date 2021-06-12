e7f0d8635888e9b41ab735c988e6d77cc7f30af0
yahavi <yahavi@jfrog.com>
2021-06-12 19:18:24 +0300

    Fix CR comments

--- artifactory/cli_test.go
+++ artifactory/cli_test.go
@@ -56,8 +56,8 @@ func TestPrepareSearchDownloadDeleteCommands(t *testing.T) {
 		{"withPattern", []string{"TestPattern"}, []string{}, "TestPattern", "", "", false},
 		{"withBuild", []string{}, []string{"build=buildName/buildNumber"}, "", "buildName/buildNumber", "", false},
 		{"withBundle", []string{}, []string{"bundle=bundleName/bundleVersion"}, "", "", "bundleName/bundleVersion", false},
-		{"withSpec", []string{}, []string{"spec=" + getSpecPath(tests.SearchAllRepo1)}, "${REPO1}/*", "", "", false},
-		{"withSpecAndPattern", []string{"TestPattern"}, []string{"spec=" + getSpecPath(tests.SearchAllRepo1)}, "", "", "", true},
+		{"withSpec", []string{}, []string{"spec=" + getSpecPath(t, tests.SearchAllRepo1)}, "${REPO1}/*", "", "", false},
+		{"withSpecAndPattern", []string{"TestPattern"}, []string{"spec=" + getSpecPath(t, tests.SearchAllRepo1)}, "", "", "", true},
 		{"withBuildAndPattern", []string{"TestPattern"}, []string{"build=buildName/buildNumber"}, "TestPattern", "buildName/buildNumber", "", false},
 	}
 
@@ -69,12 +69,7 @@ func TestPrepareSearchDownloadDeleteCommands(t *testing.T) {
 			}
 			for _, prepareCommandFunc := range funcArray {
 				specFiles, err := prepareCommandFunc(context)
-				if test.expectError {
-					assert.Error(t, err, buffer)
-				} else {
-					assert.NoError(t, err, buffer)
-					assert.Equal(t, test.expectedPattern, specFiles.Get(0).Pattern)
-				}
+				assertGenericCommand(t, err, buffer, test.expectError, test.expectedPattern, test.expectedBuild, test.expectedBundle, specFiles)
 			}
 		})
 	}
@@ -93,8 +88,8 @@ func TestPrepareCopyMoveCommand(t *testing.T) {
 	}{
 		{"withoutArguments", []string{}, []string{}, "", "", "", "", true},
 		{"withPatternAndTarget", []string{"TestPattern", "TestTarget"}, []string{}, "TestPattern", "TestTarget", "", "", false},
-		{"withSpec", []string{}, []string{"spec=" + getSpecPath(tests.CopyItemsSpec)}, "${REPO1}/*/", "${REPO2}/", "", "", false},
-		{"withSpecAndPattern", []string{"TestPattern"}, []string{"spec=" + getSpecPath(tests.CopyItemsSpec)}, "", "", "", "", true},
+		{"withSpec", []string{}, []string{"spec=" + getSpecPath(t, tests.CopyItemsSpec)}, "${REPO1}/*/", "${REPO2}/", "", "", false},
+		{"withSpecAndPattern", []string{"TestPattern"}, []string{"spec=" + getSpecPath(t, tests.CopyItemsSpec)}, "", "", "", "", true},
 		{"withPatternTargetAndBuild", []string{"TestPattern", "TestTarget"}, []string{"build=buildName/buildNumber"}, "TestPattern", "", "buildName/buildNumber", "", false},
 		{"withPatternTargetAndBundle", []string{"TestPattern", "TestTarget"}, []string{"bundle=bundleName/bundleVersion"}, "TestPattern", "", "", "bundleName/bundleVersion", false},
 	}
@@ -103,12 +98,7 @@ func TestPrepareCopyMoveCommand(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			context, buffer := createContext(test.flags, test.args)
 			specFiles, err := prepareCopyMoveCommand(context)
-			if test.expectError {
-				assert.Error(t, err, buffer)
-			} else {
-				assert.NoError(t, err, buffer)
-				assert.Equal(t, test.expectedPattern, specFiles.Get(0).Pattern)
-			}
+			assertGenericCommand(t, err, buffer, test.expectError, test.expectedPattern, test.expectedBuild, test.expectedBundle, specFiles)
 		})
 	}
 }
@@ -128,8 +118,8 @@ func TestPreparePropsCmd(t *testing.T) {
 		{"withPattern", []string{"TestPattern", "key1=val1"}, []string{}, "key1=val1", "TestPattern", "", "", false},
 		{"withBuild", []string{"key1=val1"}, []string{"build=buildName/buildNumber"}, "key1=val1", "*", "buildName/buildNumber", "", false},
 		{"withBundle", []string{"key1=val1"}, []string{"bundle=bundleName/bundleVersion"}, "key1=val1", "*", "", "bundleName/bundleVersion", false},
-		{"withSpec", []string{"key1=val1"}, []string{"spec=" + getSpecPath(tests.SetDeletePropsSpec)}, "key1=val1", "${REPO1}/", "", "", false},
-		{"withSpecAndPattern", []string{"TestPattern", "key1=val1"}, []string{"spec=" + getSpecPath(tests.SetDeletePropsSpec)}, "key1=val1", "", "", "", true},
+		{"withSpec", []string{"key1=val1"}, []string{"spec=" + getSpecPath(t, tests.SetDeletePropsSpec)}, "key1=val1", "${REPO1}/", "", "", false},
+		{"withSpecAndPattern", []string{"TestPattern", "key1=val1"}, []string{"spec=" + getSpecPath(t, tests.SetDeletePropsSpec)}, "key1=val1", "", "", "", true},
 		{"withPatternAndBuild", []string{"TestPattern", "key1=val1"}, []string{"build=buildName/buildNumber"}, "key1=val1", "TestPattern", "buildName/buildNumber", "", false},
 	}
 
@@ -137,14 +127,24 @@ func TestPreparePropsCmd(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			context, buffer := createContext(test.flags, test.args)
 			propsCommand, err := preparePropsCmd(context)
-			if test.expectError {
-				assert.Error(t, err, buffer)
-			} else {
-				assert.NoError(t, err, buffer)
+			var actualSpec *spec.SpecFiles
+			if !test.expectError {
+				actualSpec = propsCommand.Spec()
 				assert.Equal(t, test.expectedProps, propsCommand.Props())
-				assert.Equal(t, test.expectedPattern, propsCommand.Spec().Get(0).Pattern)
 			}
+			assertGenericCommand(t, err, buffer, test.expectError, test.expectedPattern, test.expectedBuild, test.expectedBundle, actualSpec)
 		})
+	}
+}
+
+func assertGenericCommand(t *testing.T, err error, buffer *bytes.Buffer, expectError bool, expectedPattern, expectedBuild, expectedBundle string, actualSpec *spec.SpecFiles) {
+	if expectError {
+		assert.Error(t, err, buffer)
+	} else {
+		assert.NoError(t, err, buffer)
+		assert.Equal(t, expectedPattern, actualSpec.Get(0).Pattern)
+		assert.Equal(t, expectedBuild, actualSpec.Get(0).Build)
+		assert.Equal(t, expectedBundle, actualSpec.Get(0).Bundle)
 	}
 }
 
@@ -159,8 +159,9 @@ func createContext(testFlags, testArgs []string) (*cli.Context, *bytes.Buffer) {
 	return cli.NewContext(app, flagSet, nil), buffer
 }
 
-func getSpecPath(spec string) string {
-	dir, _ := os.Getwd()
+func getSpecPath(t *testing.T, spec string) string {
+	dir, err := os.Getwd()
+	assert.NoError(t, err)
 	return filepath.Join(path.Dir(dir), "testdata", "filespecs", spec)
 }
 
@@ -171,5 +172,7 @@ func setStringFlags(flagSet *flag.FlagSet, flags ...string) []string {
 		flagSet.String(strings.Split(flag, "=")[0], "", "")
 		cmdFlags = append(cmdFlags, "--"+flag)
 	}
+	flagSet.Parse(cmdFlags)
+
 	return cmdFlags
 }
