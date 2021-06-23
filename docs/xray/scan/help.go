c8f904bc22969b07ca8573e61f49aa56c40b5102
Tal Arian <tala@jfrog.com>
2021-06-23 18:52:32 +0300

    Update scan command docs

--- docs/xray/scan/help.go
+++ docs/xray/scan/help.go
@@ -2,4 +2,10 @@ package scan
 
 const Description = "Execute an Xray scan command on the binaries found according to the given file spec, using the configured Xray details. "
 
-var Usage = []string{`jfrog xr scan  --spec=<spec file> [command options]`}
+var Usage = []string{"jfrog xr scan [command options] <source pattern> ",
+	`jfrog xr scan [command options] --spec=<spec file> [command options]`}
+
+const Arguments string = `	source pattern
+		Specifies the local file system path to binaries which should be scan by Xray.
+		You can specify multiple files by using wildcards, Ant pattern or a regular expression.
+		If you have specified that you are using regular expressions, then the first one used in the argument must be enclosed in parenthesis.`
