88e110115ca0418c37fb173d0583de6f9e4cb5f5
Tal Arian <tala@jfrog.com>
2021-07-28 12:57:34 +0300

    Fix Tests

--- main_test.go
+++ main_test.go
@@ -99,6 +99,7 @@ func createJfrogHomeConfig(t *testing.T, encryptPassword bool) {
 	if err == nil && config.ServerId != "" {
 		err = tests.NewJfrogCli(execMain, "jfrog config", "").Exec("rm", "default", "--quiet")
 	}
+	*tests.JfrogUrl = utils.AddTrailingSlashIfNeeded(*tests.JfrogUrl)
 	err = tests.NewJfrogCli(execMain, "jfrog config", credentials).Exec("add", "default", "--interactive=false", "--artifactory-url="+*tests.JfrogUrl+tests.ArtifactoryDomain, "--enc-password="+strconv.FormatBool(encryptPassword))
 	assert.NoError(t, err)
 }
