4c45796e7e6906a04f13ab0a3d882b6ead4c0855
sverdlov93 <sverdlov93@gmail.com>
2021-10-27 17:52:19 +0300

    fix test
    Signed-off-by: sverdlov93 <sverdlov93@gmail.com>

--- artifactory_test.go
+++ artifactory_test.go
@@ -4987,6 +4987,10 @@ func assertPermissionTarget(t *testing.T, manager artifactory.ArtifactoryService
 		assert.NoError(t, err)
 		return
 	}
+	if actual == nil {
+		assert.NotNil(t, actual)
+		return
+	}
 	expected := tests.GetExpectedPermissionTarget(repoValue)
 	assert.EqualValues(t, expected, *actual)
 }
