ff21158b9bff562c45d5177b1848b3dd6cf6a86c
sverdlov93 <sverdlov93@gmail.com>
2021-12-22 18:31:47 +0200

    fixes after CR
    Signed-off-by: sverdlov93 <sverdlov93@gmail.com>

--- docs/xray/auditpipenv/help.go
+++
@@ -1,7 +0,0 @@
-package auditpipenv
-
-var Usage = []string{"xr audit-pipenv [command options]"}
-
-func GetDescription() string {
-	return "Execute an audit Pipenv command, using the configured Xray details."
-}
