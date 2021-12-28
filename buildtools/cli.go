d59c6447d73948b9448b908bad304162784a66cd
gailazar300 <gail@jfrog.com>
2021-12-28 15:56:49 +0200

    fix cli

--- buildtools/cli.go
+++ buildtools/cli.go
@@ -3,6 +3,9 @@ package buildtools
 import (
 	"errors"
 	"fmt"
+	"github.com/jfrog/jfrog-cli-core/v2/artifactory/commands/terraform"
+	terraformdocs "github.com/jfrog/jfrog-cli/docs/artifactory/terraform"
+	"github.com/jfrog/jfrog-cli/docs/artifactory/terraformconfig"
 	"os"
 	"strconv"
 	"strings"
@@ -305,6 +308,33 @@ func GetCommands() []cli.Command {
 			Category:        buildToolsCategory,
 			Action: func(c *cli.Context) error {
 				return npmCmd(c)
+			},
+		},
+		{
+			Name:         "terraform-config",
+			Flags:        cliutils.GetCommandFlags(cliutils.TerraformConfig),
+			Aliases:      []string{"terraformc"},
+			Description:  terraformconfig.GetDescription(),
+			HelpName:     corecommon.CreateUsage("terraform-config", terraformconfig.GetDescription(), terraformconfig.Usage),
+			ArgsUsage:    common.CreateEnvVars(),
+			BashComplete: corecommon.CreateBashCompletionFunc(),
+			Category:     buildToolsCategory,
+			Action: func(c *cli.Context) error {
+				return cliutils.CreateConfigCmd(c, utils.Terraform)
+			},
+		},
+		{
+			Name:         "terraform",
+			Flags:        cliutils.GetCommandFlags(cliutils.Terraform),
+			Aliases:      []string{"tf"},
+			Description:  terraformdocs.GetDescription(),
+			HelpName:     corecommon.CreateUsage("terraform", terraformdocs.GetDescription(), terraformdocs.Usage),
+			UsageText:    terraformdocs.GetArguments(),
+			ArgsUsage:    common.CreateEnvVars(),
+			BashComplete: corecommon.CreateBashCompletionFunc(),
+			Category:     buildToolsCategory,
+			Action: func(c *cli.Context) error {
+				return terraformCmd(c)
 			},
 		},
 	})
@@ -770,4 +800,48 @@ func pythonNativeCmd(cmdName string, rtDetails *coreConfig.ServerDetails, target
 	default:
 		return errors.New(fmt.Sprintf("python project type: %s is currently not supported", projectType.String()))
 	}
+}
+
+func terraformCmd(c *cli.Context) error {
+	configFilePath, orgArgs, err := getTerraformConfigAndArgs(c)
+	if err != nil {
+		return err
+	}
+	cmdName, filteredArgs := getCommandName(orgArgs)
+	switch cmdName {
+	// Aliases accepted by npm.
+	case "publish", "p":
+		return terraformPublishCmd(configFilePath, filteredArgs, c)
+	default:
+		return errorutils.CheckError(errors.New("Terraform command:\"" + cmdName + "\" is not supported. " + cliutils.GetDocumentationMessage()))
+	}
+}
+
+func getTerraformConfigAndArgs(c *cli.Context) (configFilePath string, args []string, err error) {
+	if show, err := cliutils.ShowCmdHelpIfNeeded(c, c.Args()); show || err != nil {
+		return "", nil, err
+	}
+
+	configFilePath, exists, err := utils.GetProjectConfFilePath(utils.Terraform)
+	if err != nil {
+		return "", nil, err
+	}
+
+	if !exists {
+		return "", nil, errors.New("No config file was found! Before running the terraform command on a project for the first time, the project should be configured using the terraform-config command. ")
+	}
+	args = cliutils.ExtractCommand(c)
+	return
+}
+
+func terraformPublishCmd(configFilePath string, args []string, c *cli.Context) error {
+	artDetails, err := cliutils.CreateArtifactoryDetailsByFlags(c)
+	if err != nil {
+		return err
+	}
+	terraformCmd := terraform.NewTerraformPublishCommand()
+	terraformCmd.SetConfigFilePath(configFilePath).SetArgs(args).SetServerDetails(artDetails)
+	err = commands.Exec(terraformCmd)
+	result := terraformCmd.Result()
+	return cliutils.PrintBriefSummaryReport(result.SuccessCount(), result.FailCount(), cliutils.IsFailNoOp(c), err)
 }
