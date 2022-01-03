6c29896a76d7f66968b18e330bc0efeda04d8a8e
matteosilv <matteosilv@gmail.com>
2022-01-03 12:46:35 +0100

    Add --fail-fast option to bpr (#1070)

--- artifactory/cli.go
+++ artifactory/cli.go
@@ -2442,6 +2442,7 @@ func createBuildPromoteConfiguration(c *cli.Context) services.PromotionParams {
 	promotionParamsImpl.Copy = c.Bool("copy")
 	promotionParamsImpl.Properties = c.String("props")
 	promotionParamsImpl.ProjectKey = c.String("project")
+	promotionParamsImpl.FailFast = c.Bool("fail-fast")
 
 	// If the command received 3 args, read the build name, build number
 	// and target repo as ags.
