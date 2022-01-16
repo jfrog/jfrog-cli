6b2c34716094d37c95a03f9d0da1b29519d1f51c
Tal Arian <tala@jfrog.com>
2022-01-16 14:05:22 +0200

    Build.sh - print success message only on success build

--- build/build.sh
+++ build/build.sh
@@ -7,5 +7,7 @@ if [ $# -eq 0 ]
 	exe_name="$1"
 fi
 
-CGO_ENABLED=0 go build -o $exe_name -ldflags '-w -extldflags "-static"' main.go
-echo "The $exe_name executable was successfully created."
+if CGO_ENABLED=0 go build -o $exe_name -ldflags '-w -extldflags "-static"' main.go
+  then
+  echo "The $exe_name executable was successfully created."
+fi
