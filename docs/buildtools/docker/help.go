7351cb297127552abe3dc81de7fae04a632c3302
Or Geva <orgab@jfrog.com>
2022-02-24 12:15:49 +0200

    Update docker help output

--- docs/buildtools/docker/help.go
+++ docs/buildtools/docker/help.go
@@ -3,12 +3,11 @@ package docker
 var Usage = []string{"docker <docker arguments> [command options]"}
 
 func GetDescription() string {
-	return `Run Docker command.
-		Tip: Use 'docker scan <image tag>' to scan a local docker container for security vulnerabilities with JFrog Xray.`
+	return `Run any docker command, including ‘jf docker scan’ for scanning a local image with Xray.`
 }
 
 func GetArguments() string {
 	return `	push                        Run docker push.
 	pull                        Run docker pull.
-	scan                        Scan a local docker container for security vulnerabilities with JFrog Xray.`
+	scan                        Scan a local docker image for security vulnerabilities with JFrog Xray.`
 }
