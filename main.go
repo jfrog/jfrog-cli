e41192c2c1be27b6a070c3fa39ddeb1751220ae4
Tal Arian <tala@jfrog.com>
2022-03-09 22:47:49 +0200

    Add audit ability for frobot

--- main.go
+++ main.go
@@ -5,30 +5,32 @@ import (
 	"fmt"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/agnivade/levenshtein"
 	"github.com/jfrog/froggit-go/vcsclient"
 	"github.com/jfrog/froggit-go/vcsutils"
-	"github.com/jfrog/jfrog-cli/distribution"
-	"github.com/jfrog/jfrog-cli/scan"
-
 	corecommon "github.com/jfrog/jfrog-cli-core/v2/docs/common"
+	coreconfig "github.com/jfrog/jfrog-cli-core/v2/utils/config"
 	"github.com/jfrog/jfrog-cli-core/v2/utils/coreutils"
 	"github.com/jfrog/jfrog-cli-core/v2/utils/log"
+	audit "github.com/jfrog/jfrog-cli-core/v2/xray/commands/audit/generic"
+	xrayutils "github.com/jfrog/jfrog-cli-core/v2/xray/utils"
+	"github.com/jfrog/jfrog-cli/artifactory"
+	"github.com/jfrog/jfrog-cli/buildtools"
+	"github.com/jfrog/jfrog-cli/completion"
 	"github.com/jfrog/jfrog-cli/config"
+	"github.com/jfrog/jfrog-cli/distribution"
 	"github.com/jfrog/jfrog-cli/docs/common"
 	"github.com/jfrog/jfrog-cli/docs/general/cisetup"
 	cisetupcommand "github.com/jfrog/jfrog-cli/general/cisetup"
 	"github.com/jfrog/jfrog-cli/general/envsetup"
 	"github.com/jfrog/jfrog-cli/general/project"
+	"github.com/jfrog/jfrog-cli/missioncontrol"
 	"github.com/jfrog/jfrog-cli/plugins"
 	"github.com/jfrog/jfrog-cli/plugins/utils"
-
-	"github.com/jfrog/jfrog-cli/artifactory"
-	"github.com/jfrog/jfrog-cli/buildtools"
-	"github.com/jfrog/jfrog-cli/completion"
-	"github.com/jfrog/jfrog-cli/missioncontrol"
+	"github.com/jfrog/jfrog-cli/scan"
 	"github.com/jfrog/jfrog-cli/utils/cliutils"
 	"github.com/jfrog/jfrog-cli/xray"
 	clientutils "github.com/jfrog/jfrog-client-go/utils"
@@ -264,40 +266,74 @@ GLOBAL OPTIONS:
 }
 
 func frobotCmd(c *cli.Context) error {
-	// The VCS provider. Cannot be changed.
-	vcsProvider := vcsutils.GitHub
-	// API endpoint to GitHub. Leave empty to use the default - https://api.github.com
-	apiEndpoint := ""
-	// Access token to GitHub
-	token := c.Args().Get(1)
+	repoOwner, token, repo, targetBranch, pullRequestID, err := extractFrobotArgs(c)
+	if err != nil {
+		return err
+	}
+	client, err := vcsclient.NewClientBuilder(vcsutils.GitHub).Token(token).Build()
+	if err != nil {
+		return err
+	}
 
-	client, err := vcsclient.NewClientBuilder(vcsProvider).ApiEndpoint(apiEndpoint).Token(token).Build()
-
-	// Go context
-	ctx := context.Background()
-	// Organization or username
-	owner := c.Args().Get(0)
-	// VCS repository
-	repository := "jfrog-cli"
-
-	content := `The full scan results are available
-	Note: no context was provided, so no policy could be determined to scan against.
-	You can get a list of custom violations by providing one of the command options: --watches, --repo-path or --project.
-	Read more about configuring Xray policies here: https://www.jfrog.com/confluence/display/JFROG/Creating+Xray+Policies+and+Rules
-	Below are all vulnerabilities detected.
-	Vulnerabilities
-	
-| SEVERITY | IMPACTED PACKAGE | IMPACTED PACKAGE  VERSION | TYPE | FIXED VERSIONS | COMPONENT | COMPONENT VERSION | CVE |
-| --- | --- |  --- | --- |  --- | --- | --- | --- |
-| High | github.com/mholt/archiver/v3 | v3.5.1-0.20210618180617-81fac4ba96e4 | Go | | github.com/jfrog/jfrog-client-go github.com/jfrog/jfrog-cli-core/v2 | v1.8.0 |
-`
-
-	err = client.AddPullRequestComment(ctx, owner, repository, content, 1)
-	return err
+	// Audit PR code
+	// TODO - fill server details according to env/flags
+	server := coreconfig.ServerDetails{Url: "https://ecosysjfrog.jfrog.io/"}
+	// TODO - fill contex according to env/flags
+	xrayScanParams := services.XrayGraphScanParams{}
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	currentScan, err := runAudit(xrayScanParams, &server, wd)
+	// Audit target code
+	previousScan, err := auditTarget(client, xrayScanParams, &server, repoOwner, repo, targetBranch)
+	if err != nil {
+		return err
+	}
+	// Get only the new issues added by this PR
+	violations := getNewViolations(previousScan[0], currentScan[0]) // TODO - handle array of scan results!
+	// Comment Frobot message on the PR
+	message := createPullRequestMessage(violations)
+	return client.AddPullRequestComment(context.Background(), repoOwner, repo, message, pullRequestID)
 
 }
 
-func auditTarget(client vcsclient.VcsClient, owner, repo, branch string) (res services.ScanResponse, err error) {
+func extractFrobotArgs(c *cli.Context) (repoOwner, token, repo, targetBranch string, pullRequestID int, err error) {
+	if c.NArg() != 5 {
+		err = fmt.Errorf("Wrong number of args")
+		return
+	}
+	repoOwner = c.Args().Get(0)
+	token = c.Args().Get(1)
+	repo = c.Args().Get(2)
+	targetBranch = c.Args().Get(3)
+	pullRequestID, err = strconv.Atoi(c.Args().Get(4))
+	if err != nil {
+		return
+	}
+	return
+}
+
+func runAudit(xrayScanParams services.XrayGraphScanParams, server *coreconfig.ServerDetails, workDir string) (res []services.ScanResponse, err error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return
+	}
+	err = os.Chdir(workDir)
+	if err != nil {
+		return
+	}
+	defer func(originDir string) {
+		e := os.Chdir(originDir)
+		if err == nil {
+			err = e
+		}
+	}(wd)
+	// TODO - handle audit params better
+	return audit.GenericAudit(xrayScanParams, server, false, false, false, []string{})
+}
+
+func auditTarget(client vcsclient.VcsClient, xrayScanParams services.XrayGraphScanParams, server *coreconfig.ServerDetails, owner, repo, branch string) (res []services.ScanResponse, err error) {
 	// First download the target repo to temp dir
 	tempWorkdir, err := fileutils.CreateTempDir()
 	if err != nil {
@@ -305,5 +341,80 @@ func auditTarget(client vcsclient.VcsClient, owner, repo, branch string) (res se
 	}
 	defer fileutils.RemoveTempDir(tempWorkdir)
 	err = client.DownloadRepository(context.Background(), owner, repo, branch, tempWorkdir)
+	return runAudit(xrayScanParams, server, tempWorkdir)
+}
+
+func getNewViolations(previousScan, currentScan services.ScanResponse) (newViolationsRows []xrayutils.VulnerabilityRow) {
+	existsViolationsMap := make(map[string]xrayutils.VulnerabilityRow)
+	violationsRows, _, err := xrayutils.CreateViolationsRows(previousScan.Violations, false, false)
+	if err != nil {
+		return
+	}
+	for _, violation := range violationsRows {
+		existsViolationsMap[GetUniqueID(violation)] = violation
+	}
+	violationsRows, _, err = xrayutils.CreateViolationsRows(currentScan.Violations, false, false)
+	if err != nil {
+		return
+	}
+	for _, violation := range violationsRows {
+		if _, exists := existsViolationsMap[GetUniqueID(violation)]; !exists {
+			newViolationsRows = append(newViolationsRows, violation)
+		}
+	}
 	return
+}
+
+func getNewVulnerabilities(previousScan, currentScan services.ScanResponse) (newVulnerabilitiesRows []xrayutils.VulnerabilityRow) {
+	existsVulnerabilitiesMap := make(map[string]xrayutils.VulnerabilityRow)
+	vulnerabilitiesRows, err := xrayutils.CreateVulnerabilitiesRows(previousScan.Vulnerabilities, false, false)
+	if err != nil {
+		return
+	}
+	for _, vulnerability := range vulnerabilitiesRows {
+		existsVulnerabilitiesMap[GetUniqueID(vulnerability)] = vulnerability
+	}
+	vulnerabilitiesRows, err = xrayutils.CreateVulnerabilitiesRows(currentScan.Vulnerabilities, false, false)
+	if err != nil {
+		return
+	}
+	for _, vulnerability := range vulnerabilitiesRows {
+		if _, exists := existsVulnerabilitiesMap[GetUniqueID(vulnerability)]; !exists {
+			newVulnerabilitiesRows = append(newVulnerabilitiesRows, vulnerability)
+		}
+	}
+	return
+
+}
+
+func GetUniqueID(vulnerability xrayutils.VulnerabilityRow) string {
+	return vulnerability.IssueId + vulnerability.Components[0].Name
+
+}
+
+func createPullRequestMessage(vulnerabilitiesRows []xrayutils.VulnerabilityRow) string {
+	// DUMMY Images for tests!
+	noVulnerabilityImageTag := `<img  src="https://speedmedia.jfrog.com/08612fe1-9391-4cf3-ac1a-6dd49c36b276/https://media.jfrog.com/wp-content/uploads/2021/12/29113553/jfrog-logo-2022.svg/mxw_96,f_auto">`
+	vulnerabilityImageTag := `<img  src="https://speedmedia.jfrog.com/08612fe1-9391-4cf3-ac1a-6dd49c36b276/https://media.jfrog.com/wp-content/uploads/2019/11/20130210/Blog-post-GoCenter-04.jpg/mxw_1024,f_auto">`
+	//
+
+	if len(vulnerabilitiesRows) == 0 {
+		return noVulnerabilityImageTag
+	}
+	tableHeder := `| SEVERITY | IMPACTED PACKAGE | IMPACTED PACKAGE  VERSION | FIXED VERSIONS | COMPONENT | COMPONENT VERSION | CVE |
+	| --- | --- |  --- | --- |  --- | --- | --- | --- |`
+	tableContent := `
+
+	
+	`
+	for vulnerability := range vulnerabilitiesRows {
+		tableContent += fmt.Sprintf("| %s | %s | %s | %s | %s | %s | %s |\n", vulnerability.Severity, vulnerability.ImpactedPackageName,
+			vulnerability.ImpactedPackageVersion, vulnerability.FixedVersions, vulnerability.Components[0].Name, vulnerability.Components[0].Version, vulnerability.Cves[0].Id)
+	}
+	return vulnerabilityImageTag + tableHeder + tableContent //`| High | github.com/mholt/archiver/v3 | v3.5.1-0.20210618180617-81fac4ba96e4 | Go | | github.com/jfrog/jfrog-client-go github.com/jfrog/jfrog-cli-core/v2 | v1.8.0 |`
+}
+
+func commentPullRequestGithub(client vcsclient.VcsClient, owner, repository, content string) error {
+
+	return client.AddPullRequestComment(context.Background(), owner, repository, content, 1)
 }
