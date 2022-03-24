6ae840c79b7b371711308998b7a51e331fc614b8
Tal Arian <tala@jfrog.com>
2022-03-24 19:24:55 +0200

    Remove Frogbot logic from CLI

--- main.go
+++ main.go
@@ -1,22 +1,15 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"sort"
-	"strconv"
 	"strings"
 
 	"github.com/agnivade/levenshtein"
-	"github.com/jfrog/froggit-go/vcsclient"
-	"github.com/jfrog/froggit-go/vcsutils"
 	corecommon "github.com/jfrog/jfrog-cli-core/v2/docs/common"
-	coreconfig "github.com/jfrog/jfrog-cli-core/v2/utils/config"
 	"github.com/jfrog/jfrog-cli-core/v2/utils/coreutils"
 	"github.com/jfrog/jfrog-cli-core/v2/utils/log"
-	audit "github.com/jfrog/jfrog-cli-core/v2/xray/commands/audit/generic"
-	xrayutils "github.com/jfrog/jfrog-cli-core/v2/xray/utils"
 	"github.com/jfrog/jfrog-cli/artifactory"
 	"github.com/jfrog/jfrog-cli/buildtools"
 	"github.com/jfrog/jfrog-cli/completion"
@@ -36,7 +29,6 @@ import (
 	clientutils "github.com/jfrog/jfrog-client-go/utils"
 	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
 	clientLog "github.com/jfrog/jfrog-client-go/utils/log"
-	"github.com/jfrog/jfrog-client-go/xray/services"
 	"github.com/urfave/cli"
 )
 
@@ -211,13 +203,6 @@ func getCommands() []cli.Command {
 			},
 		},
 		{
-			Name:     "frobot",
-			HideHelp: true,
-			Hidden:   true,
-			Category: otherCategory,
-			Action:   frobotCmd,
-		},
-		{
 			Name:     "setup",
 			HideHelp: true,
 			Hidden:   true,
@@ -263,192 +248,4 @@ GLOBAL OPTIONS:
    {{end}}
 {{end}}
 `
-}
-
-func frobotCmd(c *cli.Context) error {
-	server, repoOwner, token, repo, targetBranch, pullRequestID, err := extractParamsFromEnv()
-	if err != nil {
-		return err
-	}
-	client, err := vcsclient.NewClientBuilder(vcsutils.GitHub).Token(token).Build()
-	if err != nil {
-		return err
-	}
-
-	// Audit PR code
-	// TODO - fill contex according to env/flags
-	xrayScanParams := services.XrayGraphScanParams{}
-	wd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	currentScan, err := runAudit(xrayScanParams, &server, wd)
-	// Audit target code
-	previousScan, err := auditTarget(client, xrayScanParams, &server, repoOwner, repo, targetBranch)
-	if err != nil {
-		return err
-	}
-	// Get only the new issues added by this PR
-	violations := getNewViolations(previousScan[0], currentScan[0]) // TODO - handle array of scan results!
-	// Comment Frobot message on the PR
-	message := createPullRequestMessage(violations)
-	return client.AddPullRequestComment(context.Background(), repoOwner, repo, message, pullRequestID)
-
-}
-
-func extractParamsFromEnv() (server coreconfig.ServerDetails, repoOwner, token, repo, targetBranch string, pullRequestID int, err error) {
-	// Constants
-	jfrogUser := "FROBOT_JF_USER"
-	jfrogUrl := "FROBOT_JF_URL"
-	jfrogPassword := "FROBOT_JF_PASSWORD"
-	jfrogToken := "FROBOT_JF_TOKEN"
-	gitRepoOwner := "FROBOT_GIT_OWNER"
-	gitToken := "FROBOT_GIT_TOKEN"
-	branch := "FROBOT_BRANCH"
-	prID := "FROBOT_PR"
-
-	url, exists := os.LookupEnv(jfrogUrl)
-	if !exists {
-		err = fmt.Errorf("%s is missing", jfrogUrl)
-		return
-	}
-	server.Url = url
-	password, passwordExists := os.LookupEnv(jfrogPassword)
-	user, userExists := os.LookupEnv(jfrogUser)
-	if passwordExists && userExists {
-		server.User = user
-		server.Password = password
-	} else if accessToken, exists := os.LookupEnv(jfrogToken); exists {
-		server.AccessToken = accessToken
-	} else {
-		err = fmt.Errorf("%s and %s or %s are missing", jfrogUser, jfrogPassword, jfrogToken)
-		return
-	}
-	if repoOwner, exists = os.LookupEnv(gitRepoOwner); !exists {
-		err = fmt.Errorf("%s is missing", gitRepoOwner)
-		return
-	}
-	if token, exists = os.LookupEnv(gitToken); !exists {
-		err = fmt.Errorf("%s is missing", gitToken)
-		return
-	}
-	if targetBranch, exists = os.LookupEnv(branch); !exists {
-		err = fmt.Errorf("%s is missing", branch)
-		return
-	}
-	pullRequestIDString, exists := os.LookupEnv(prID)
-	if !exists {
-		err = fmt.Errorf("%s is missing", prID)
-		return
-	}
-	pullRequestID, err = strconv.Atoi(pullRequestIDString)
-	if err != nil {
-		return
-	}
-	return
-}
-
-func runAudit(xrayScanParams services.XrayGraphScanParams, server *coreconfig.ServerDetails, workDir string) (res []services.ScanResponse, err error) {
-	wd, err := os.Getwd()
-	if err != nil {
-		return
-	}
-	err = os.Chdir(workDir)
-	if err != nil {
-		return
-	}
-	defer func(originDir string) {
-		e := os.Chdir(originDir)
-		if err == nil {
-			err = e
-		}
-	}(wd)
-	// TODO - handle audit params better
-	return audit.GenericAudit(xrayScanParams, server, false, false, false, []string{})
-}
-
-func auditTarget(client vcsclient.VcsClient, xrayScanParams services.XrayGraphScanParams, server *coreconfig.ServerDetails, owner, repo, branch string) (res []services.ScanResponse, err error) {
-	// First download the target repo to temp dir
-	tempWorkdir, err := fileutils.CreateTempDir()
-	if err != nil {
-		return
-	}
-	defer fileutils.RemoveTempDir(tempWorkdir)
-	err = client.DownloadRepository(context.Background(), owner, repo, branch, tempWorkdir)
-	return runAudit(xrayScanParams, server, tempWorkdir)
-}
-
-func getNewViolations(previousScan, currentScan services.ScanResponse) (newViolationsRows []xrayutils.VulnerabilityRow) {
-	existsViolationsMap := make(map[string]xrayutils.VulnerabilityRow)
-	violationsRows, _, err := xrayutils.CreateViolationsRows(previousScan.Violations, false, false)
-	if err != nil {
-		return
-	}
-	for _, violation := range violationsRows {
-		existsViolationsMap[GetUniqueID(violation)] = violation
-	}
-	violationsRows, _, err = xrayutils.CreateViolationsRows(currentScan.Violations, false, false)
-	if err != nil {
-		return
-	}
-	for _, violation := range violationsRows {
-		if _, exists := existsViolationsMap[GetUniqueID(violation)]; !exists {
-			newViolationsRows = append(newViolationsRows, violation)
-		}
-	}
-	return
-}
-
-func getNewVulnerabilities(previousScan, currentScan services.ScanResponse) (newVulnerabilitiesRows []xrayutils.VulnerabilityRow) {
-	existsVulnerabilitiesMap := make(map[string]xrayutils.VulnerabilityRow)
-	vulnerabilitiesRows, err := xrayutils.CreateVulnerabilitiesRows(previousScan.Vulnerabilities, false, false)
-	if err != nil {
-		return
-	}
-	for _, vulnerability := range vulnerabilitiesRows {
-		existsVulnerabilitiesMap[GetUniqueID(vulnerability)] = vulnerability
-	}
-	vulnerabilitiesRows, err = xrayutils.CreateVulnerabilitiesRows(currentScan.Vulnerabilities, false, false)
-	if err != nil {
-		return
-	}
-	for _, vulnerability := range vulnerabilitiesRows {
-		if _, exists := existsVulnerabilitiesMap[GetUniqueID(vulnerability)]; !exists {
-			newVulnerabilitiesRows = append(newVulnerabilitiesRows, vulnerability)
-		}
-	}
-	return
-
-}
-
-func GetUniqueID(vulnerability xrayutils.VulnerabilityRow) string {
-	return vulnerability.IssueId + vulnerability.Components[0].Name
-
-}
-
-func createPullRequestMessage(vulnerabilitiesRows []xrayutils.VulnerabilityRow) string {
-	// DUMMY Images for tests!
-	noVulnerabilityImageTag := `<img  src="https://speedmedia.jfrog.com/08612fe1-9391-4cf3-ac1a-6dd49c36b276/https://media.jfrog.com/wp-content/uploads/2021/12/29113553/jfrog-logo-2022.svg/mxw_96,f_auto">`
-	vulnerabilityImageTag := `<img  src="https://speedmedia.jfrog.com/08612fe1-9391-4cf3-ac1a-6dd49c36b276/https://media.jfrog.com/wp-content/uploads/2019/11/20130210/Blog-post-GoCenter-04.jpg/mxw_1024,f_auto">`
-	//
-
-	if len(vulnerabilitiesRows) == 0 {
-		return noVulnerabilityImageTag
-	}
-	tableHeder := `| SEVERITY | IMPACTED PACKAGE | IMPACTED PACKAGE  VERSION | FIXED VERSIONS | COMPONENT | COMPONENT VERSION | CVE |
-	| --- | --- |  --- | --- |  --- | --- | --- | --- |`
-	tableContent := `
-
-	
-	`
-	for _, vulnerability := range vulnerabilitiesRows {
-		tableContent += fmt.Sprintf("| %s | %s | %s | %s | %s | %s | %s |\n", vulnerability.Severity, vulnerability.ImpactedPackageName,
-			vulnerability.ImpactedPackageVersion, vulnerability.FixedVersions, vulnerability.Components[0].Name, vulnerability.Components[0].Version, vulnerability.Cves[0].Id)
-	}
-	return vulnerabilityImageTag + tableHeder + tableContent //`| High | github.com/mholt/archiver/v3 | v3.5.1-0.20210618180617-81fac4ba96e4 | Go | | github.com/jfrog/jfrog-client-go github.com/jfrog/jfrog-cli-core/v2 | v1.8.0 |`
-}
-
-func commentPullRequestGithub(client vcsclient.VcsClient, owner, repository, content string) error {
-
-	return client.AddPullRequestComment(context.Background(), owner, repository, content, 1)
 }
