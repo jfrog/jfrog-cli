f154e65a64cdaf4bef6ca57d4eb4aacae1a74bbc
Asaf Gabai <77976014+asafgabai@users.noreply.github.com>
2022-05-08 19:20:57 +0300

    Setup command for Docker Desktop extension (#1528)
    * Added --format option to the setup command.

--- general/envsetup/envsetup.go
+++ general/envsetup/envsetup.go
@@ -11,12 +11,14 @@ const (
 	registrationPageURL = "https://jfrog.com/start-free/cli/"
 )
 
-func RunEnvSetupCmd() error {
-	log.Output()
-	log.Output()
-	log.Output(coreutils.PrintTitle("Thank you for installing JFrog CLI! 🐸"))
-	log.Output(coreutils.PrintTitle("We'll now set up a FREE JFrog environment in the cloud for you, and configure your local machine to use it."))
-	log.Output("Your environment will be ready in less than a minute.")
-	setupCmd := envsetup.NewEnvSetupCommand(registrationPageURL)
+func RunEnvSetupCmd(outputFormat envsetup.OutputFormat) error {
+	if outputFormat == envsetup.Human {
+		log.Output()
+		log.Output()
+		log.Output(coreutils.PrintTitle("Thank you for installing JFrog CLI! 🐸"))
+		log.Output(coreutils.PrintTitle("We'll now set up a FREE JFrog environment in the cloud for you, and configure your local machine to use it."))
+		log.Output("Your environment will be ready in less than a minute.")
+	}
+	setupCmd := envsetup.NewEnvSetupCommand(registrationPageURL).SetOutputFormat(outputFormat)
 	return progressbar.ExecWithProgress(setupCmd, false)
 }
