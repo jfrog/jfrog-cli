1d3867e51be67a2e1c91419dfedb7f1e824a74ee
Omer Zidkoni <50792403+omerzi@users.noreply.github.com>
2022-07-25 17:43:37 +0300

    Added Intro Command (#1580)
    * Added 'intro' command

--- utils/cliutils/commandsflags.go
+++ utils/cliutils/commandsflags.go
@@ -11,6 +11,7 @@ import (
 const (
 	// CLI base commands keys
 	Setup = "setup"
+	Intro = "intro"
 
 	// Artifactory's Commands Keys
 	DeleteConfig           = "delete-config"
@@ -1693,6 +1694,7 @@ var commandFlags = map[string][]string{
 	Setup: {
 		setupFormat,
 	},
+	Intro: {},
 }
 
 func GetCommandFlags(cmd string) []cli.Flag {
