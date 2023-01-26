31851a27466ab1532f23f032becf356eff8ca8c9
Bhanu Reddy <bhanur@jfrog.com>
2023-01-26 15:24:16 +0530

    Changed sync status command to sync-status

--- docs/pipelines/syncstatus/help.go
+++ docs/pipelines/syncstatus/help.go
@@ -1,6 +1,6 @@
 package syncstatus
 
-var Usage = []string{"pl sync status"}
+var Usage = []string{"pl sync-status"}
 
 func GetDescription() string {
 	return "Fetch pipeline resource sync status."
