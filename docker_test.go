b63a465f412d8443908e1ad056af51754b7bfc48
Or Geva <orgab@jfrog.com>
2023-04-27 16:38:05 +0300

    Add retries to podman/docker tests

--- docker_test.go
+++ docker_test.go
@@ -134,7 +134,7 @@ func TestContainerPushWithDetailedSummary(t *testing.T) {
 				// Check deployment view
 				assertPrintedDeploymentViewFunc, cleanupFunc := initDeploymentViewTest(t)
 				defer cleanupFunc()
-				runRt(t, containerManager.String()+"-push", pushCommand.ImageTag(), tests.DockerLocalRepo)
+				runContainerCmd(t,artCliWrapper(containerManager.String()+"-push", pushCommand.ImageTag(), tests.DockerLocalRepo))
 				assertPrintedDeploymentViewFunc()
 				inttestutils.ContainerTestCleanup(t, serverDetails, artHttpDetails, imageName, tests.DockerBuildName, repo)
 			}()
@@ -159,9 +159,9 @@ func runPushTest(containerManager container.ContainerManagerType, imageName, mod
 	buildNumber := "1"
 
 	if withModule {
-		runRt(t, containerManager.String()+"-push", imageTag, repo, "--build-name="+tests.DockerBuildName, "--build-number="+buildNumber, "--module="+module)
+		runContainerCmd(t,artCliWrapper(containerManager.String()+"-push", imageTag, repo, "--build-name="+tests.DockerBuildName, "--build-number="+buildNumber, "--module="+module))
 	} else {
-		runRt(t, containerManager.String()+"-push", imageTag, repo, "--build-name="+tests.DockerBuildName, "--build-number="+buildNumber)
+		runContainerCmd(t,artCliWrapper(containerManager.String()+"-push", imageTag, repo, "--build-name="+tests.DockerBuildName, "--build-number="+buildNumber))
 	}
 	defer inttestutils.ContainerTestCleanup(t, serverDetails, artHttpDetails, imageName, tests.DockerBuildName, repo)
 
@@ -296,7 +296,7 @@ func TestContainerPushBuildNameNumberFromEnv(t *testing.T) {
 			setEnvCallBack = clientTestUtils.SetEnvWithCallbackAndAssert(t, coreutils.BuildNumber, buildNumber)
 			defer setEnvCallBack()
 			// Push container image
-			runRt(t, containerManager.String()+"-push", imageTag, tests.DockerLocalRepo)
+			runContainerCmd(t,artCliWrapper(containerManager.String()+"-push", imageTag, tests.DockerLocalRepo))
 			runRt(t, "build-publish")
 
 			imagePath := path.Join(tests.DockerLocalRepo, tests.DockerImageName, "1") + "/"
@@ -316,19 +316,18 @@ func TestContainerPull(t *testing.T) {
 			for _, repo := range []string{tests.DockerVirtualRepo, tests.DockerLocalRepo} {
 
 				// Push container image
-				runRt(t, containerManager.String()+"-push", imageName, repo)
-
+				runContainerCmd(t,artCliWrapper(containerManager.String()+"-push", imageName, repo))
 				buildNumber := "1"
 
 				// Pull container image
-				runRt(t, containerManager.String()+"-pull", imageName, repo, "--build-name="+tests.DockerBuildName, "--build-number="+buildNumber)
+				runContainerCmd(t,artCliWrapper(containerManager.String()+"-pull", imageName, repo, "--build-name="+tests.DockerBuildName, "--build-number="+buildNumber))
 				runRt(t, "build-publish", tests.DockerBuildName, buildNumber)
 
 				imagePath := path.Join(repo, tests.DockerImageName, "1") + "/"
 				validateContainerBuild(tests.DockerBuildName, buildNumber, imagePath, tests.DockerImageName+":1", 0, 7, 7, t)
 
 				buildNumber = "2"
-				runRt(t, containerManager.String()+"-pull", imageName, repo, "--build-name="+tests.DockerBuildName, "--build-number="+buildNumber, "--module="+ModuleNameJFrogTest)
+				runContainerCmd(t,artCliWrapper(containerManager.String()+"-pull", imageName, repo, "--build-name="+tests.DockerBuildName, "--build-number="+buildNumber, "--module="+ModuleNameJFrogTest))
 				runRt(t, "build-publish", tests.DockerBuildName, buildNumber)
 				validateContainerBuild(tests.DockerBuildName, buildNumber, imagePath, ModuleNameJFrogTest, 0, 7, 7, t)
 
@@ -354,7 +353,7 @@ func TestContainerFatManifestPull(t *testing.T) {
 				// Pull container image
 				imageTag := path.Join(*tests.ContainerRegistry, dockerRepo, imageName+":2.2")
 				defer inttestutils.DeleteTestImage(t, imageTag, containerManager)
-				runRt(t, containerManager.String()+"-pull", imageTag, dockerRepo, "--build-name="+tests.DockerBuildName, "--build-number="+buildNumber)
+				runContainerCmd(t,artCliWrapper(containerManager.String()+"-pull", imageTag, dockerRepo, "--build-name="+tests.DockerBuildName, "--build-number="+buildNumber))
 				runRt(t, "build-publish", tests.DockerBuildName, buildNumber)
 
 				// Validate
@@ -386,7 +385,7 @@ func TestDockerPromote(t *testing.T) {
 	defer inttestutils.DeleteTestImage(t, imageName, container.DockerClient)
 
 	// Push image
-	runRt(t, "docker-push", imageName, tests.DockerLocalRepo)
+	runContainerCmd(t,artCliWrapper("docker-push", imageName, tests.DockerLocalRepo))
 	assert.NoError(t, err)
 
 	// Promote image
@@ -511,7 +510,7 @@ func TestNativeDockerPushPull(t *testing.T) {
 	image, err := inttestutils.BuildTestImage(tests.DockerImageName+":"+pushBuildNumber, "", tests.DockerLocalRepo, container.DockerClient)
 	assert.NoError(t, err)
 	// Add docker cli flag '-D' to check we ignore them
-	runNativeDocker(t, "docker", "-D", "push", image, "--build-name="+tests.DockerBuildName, "--build-number="+pushBuildNumber, "--module="+module)
+	runContainerCmd(t, dockerCliWrapper("docker", "-D", "push", image, "--build-name="+tests.DockerBuildName, "--build-number="+pushBuildNumber, "--module="+module))
 
 	inttestutils.ValidateGeneratedBuildInfoModule(t, tests.DockerBuildName, pushBuildNumber, "", []string{module}, entities.Docker)
 	runRt(t, "build-publish", tests.DockerBuildName, pushBuildNumber)
@@ -519,7 +518,7 @@ func TestNativeDockerPushPull(t *testing.T) {
 	validateContainerBuild(tests.DockerBuildName, pushBuildNumber, imagePath, module, 7, 5, 7, t)
 	inttestutils.DeleteTestImage(t, image, container.DockerClient)
 
-	runNativeDocker(t, "docker", "-D", "pull", image, "--build-name="+tests.DockerBuildName, "--build-number="+pullBuildNumber, "--module="+module)
+	runContainerCmd(t, dockerCliWrapper("docker", "-D", "pull", image, "--build-name="+tests.DockerBuildName, "--build-number="+pullBuildNumber, "--module="+module))
 	runRt(t, "build-publish", tests.DockerBuildName, pullBuildNumber)
 	imagePath = path.Join(tests.DockerLocalRepo, tests.DockerImageName, pullBuildNumber) + "/"
 	validateContainerBuild(tests.DockerBuildName, pullBuildNumber, imagePath, module, 0, 7, 0, t)
@@ -544,12 +543,32 @@ func TestNativeDockerFlagParsing(t *testing.T) {
 	}
 	for _, testCase := range dockerTestCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			runNativeDocker(t, testCase.args...)
+			runContainerCmd(t, dockerCliWrapper(testCase.args...))
 		})
 	}
 }
 
-func runNativeDocker(t *testing.T, args ...string) {
-	jfCli := tests.NewJfrogCli(execMain, "jf", "")
-	assert.NoError(t, jfCli.WithoutCredentials().Exec(args...))
+func artCliWrapper(args ...string) func() error {
+	return func() error {
+		return artifactoryCli.Exec(args...)
+	}
+}
+
+func dockerCliWrapper(args ...string) func() error {
+	return func() error {
+		return tests.NewJfrogCli(execMain, "jf", "").WithoutCredentials().Exec(args...)
+	}
+}
+
+func runContainerCmd(t *testing.T, task func() error) {
+	executor := &clientUtils.RetryExecutor{
+		MaxRetries:               10,
+		RetriesIntervalMilliSecs: 3000,
+		ExecutionHandler: func() (shouldRetry bool, err error) {
+			err = task()
+			return err != nil, err
+
+		},
+	}
+	assert.NoError(t, executor.Execute())
 }
