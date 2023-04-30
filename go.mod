c381ad86d42ce04947c6a0a524f0f898219e743b
yahavi <yahavi@jfrog.com>
2023-04-30 12:42:02 +0300

    Transfer config - allow providing the src work dir

--- go.mod
+++ go.mod
@@ -121,8 +121,8 @@ require (
 
 // replace github.com/jfrog/build-info-go => github.com/jfrog/build-info-go v1.8.9-0.20230316095417-a9f6b73206d7
 
-replace github.com/jfrog/jfrog-client-go => github.com/jfrog/jfrog-client-go v1.28.1-0.20230423150914-af631338e74b
+replace github.com/jfrog/jfrog-client-go => github.com/jfrog/jfrog-client-go v1.28.1-0.20230430083747-590ae14f9dca
 
-replace github.com/jfrog/jfrog-cli-core/v2 => github.com/jfrog/jfrog-cli-core/v2 v2.31.1-0.20230427132104-0046ec524f06
+replace github.com/jfrog/jfrog-cli-core/v2 => github.com/yahavi/jfrog-cli-core/v2 v2.0.0-20230430090318-5305531a0efa
 
 replace github.com/jfrog/gofrog => github.com/jfrog/gofrog v1.2.6-0.20230418122323-2bf299dd6d27
--- utils/cliutils/commandsflags.go
+++ utils/cliutils/commandsflags.go
@@ -2,10 +2,11 @@ package cliutils
 
 import (
 	"fmt"
-	"github.com/jfrog/jfrog-cli-core/v2/utils/coreutils"
-	"github.com/jfrog/jfrog-cli-core/v2/xray/commands/offlineupdate"
 	"sort"
 	"strconv"
+
+	"github.com/jfrog/jfrog-cli-core/v2/utils/coreutils"
+	"github.com/jfrog/jfrog-cli-core/v2/xray/commands/offlineupdate"
 
 	"github.com/jfrog/jfrog-client-go/utils/log"
 	"github.com/urfave/cli"
@@ -393,9 +394,10 @@ const (
 	xrayScan = "scan"
 
 	// Unique config transfer flags
-	Force      = "force"
-	Verbose    = "verbose"
-	WorkingDir = "working-dir"
+	Force            = "force"
+	Verbose          = "verbose"
+	SourceWorkingDir = "source-working-dir"
+	TargetWorkingDir = "target-working-dir"
 
 	// *** Distribution Commands' flags ***
 	// Base flags
@@ -1150,8 +1152,12 @@ var flagsMap = map[string]cli.Flag{
 		Name:  Verbose,
 		Usage: "[Default: false] Set to true to increase verbosity during the export configuration from the source Artifactory phase.` `",
 	},
-	WorkingDir: cli.StringFlag{
-		Name:  WorkingDir,
+	SourceWorkingDir: cli.StringFlag{
+		Name:  SourceWorkingDir,
+		Usage: "[Default: $JFROG_CLI_TEMP_DIR] Local working directory on the source Artifactory server.` `",
+	},
+	TargetWorkingDir: cli.StringFlag{
+		Name:  TargetWorkingDir,
 		Usage: "[Default: '/storage'] Local working directory on the target Artifactory server.` `",
 	},
 
@@ -1704,7 +1710,7 @@ var commandFlags = map[string][]string{
 		buildName, buildNumber, module, project,
 	},
 	TransferConfig: {
-		Force, Verbose, IncludeRepos, ExcludeRepos, WorkingDir, PreChecks,
+		Force, Verbose, IncludeRepos, ExcludeRepos, SourceWorkingDir, TargetWorkingDir, PreChecks,
 	},
 	TransferConfigMerge: {
 		IncludeRepos, ExcludeRepos, IncludeProjects, ExcludeProjects,
