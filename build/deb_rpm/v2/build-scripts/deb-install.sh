4774f3f1fee5b47bd50fac3141f9067fdd03a3a6
Michael Sverdlov <michaelsv@jfrog.com>
2023-09-21 12:30:39 +0300

    fix build script
    Signed-off-by: Michael Sverdlov <michaelsv@jfrog.com>

--- build/deb_rpm/v2/build-scripts/deb-install.sh
+++ build/deb_rpm/v2/build-scripts/deb-install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget -qO - https://releases.jfrog.io/artifactory/api/gpg/key/public | apt-key add -;
 echo "deb https://releases.jfrog.io/artifactory/jfrog-debs xenial contrib" | sudo tee -a /etc/apt/sources.list;
 apt update;
--- build/deb_rpm/v2/build-scripts/rpm-install.sh
+++ build/deb_rpm/v2/build-scripts/rpm-install.sh
@@ -1,8 +1,12 @@
-echo "[jfrog-cli]" > jfrog-cli.repo;
-echo "name=jfrog-cli" >> jfrog-cli.repo;
-echo "baseurl=https://releases.jfrog.io/artifactory/jfrog-rpms" >> jfrog-cli.repo;
-echo "enabled=1" >> jfrog-cli.repo;
-echo "gpgcheck=0" >> jfrog-cli.repo;
+#!/bin/bash
+
+{
+  echo "[jfrog-cli]"
+  echo "name=jfrog-cli"
+  echo "baseurl=https://releases.jfrog.io/artifactory/jfrog-rpms"
+  echo "enabled=1"
+  echo "gpgcheck=0"
+} >> jfrog-cli.repo
 sudo mv jfrog-cli.repo /etc/yum.repos.d/;
 yum install -y jfrog-cli-v2;
 jf intro;
