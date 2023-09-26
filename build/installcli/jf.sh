b04accc7140079fe0db36fe5ddab2f6823ca27e9
Michael Sverdlov <sverdlov93@gmail.com>
2023-09-26 13:57:50 +0300

    Add Shellcheck action and fix issues, also adds npm build script tests (#2208)

--- build/installcli/jf.sh
+++ build/installcli/jf.sh
@@ -3,8 +3,6 @@
 CLI_OS="na"
 CLI_MAJOR_VER="v2-jf"
 VERSION="[RELEASE]"
-# Order is by destination priority.
-DESTINATION_PATHS="/usr/local/bin /usr/bin /opt/bin"
 
 if [ $# -eq 1 ]
 then
@@ -65,13 +63,12 @@ curl -XGET "$URL" -L -k -g > $FILE_NAME
 chmod u+x $FILE_NAME
 
 # Move executable to a destination in path.
-set -- $DESTINATION_PATHS
+# Order is by destination priority.
+set -- "/usr/local/bin" "/usr/bin" "/opt/bin"
 while [ -n "$1" ]; do
     # Check if destination is in path.
-    if echo $PATH|grep "$1" -> /dev/null ; then
-        mv $FILE_NAME $1
-        if [ "$?" -eq "0" ]
-        then
+    if echo "$PATH"|grep "$1" -> /dev/null ; then
+        if mv $FILE_NAME "$1" ; then
             echo ""
             echo "The $FILE_NAME executable was installed in $1"
             jf intro
@@ -79,8 +76,7 @@ while [ -n "$1" ]; do
         else
             echo ""
             echo "We'd like to install the JFrog CLI executable in $1. Please approve this installation by entering your password."
-            sudo mv $FILE_NAME $1
-            if [ "$?" -eq "0" ]
+            if sudo mv $FILE_NAME "$1" -eq "0"
             then
                 echo ""
                 echo "The $FILE_NAME executable was installed in $1"
