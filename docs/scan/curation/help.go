48afc793ca9f06e718a607a13cf57dc7e910ee28
Michael Sverdlov <michaelsv@jfrog.com>
2024-02-15 14:08:48 +0200

    empty archive
    Signed-off-by: Michael Sverdlov <michaelsv@jfrog.com>

--- docs/scan/curation/help.go
+++
@@ -1,7 +0,0 @@
-package curation
-
-var Usage = []string{"curation-audit [command options]"}
-
-func GetDescription() string {
-	return "Audit your project dependencies for their curation status."
-}
