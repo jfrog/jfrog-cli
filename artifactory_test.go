caa7817b2df56d25a08765d1c432da8dafaf0dfa
delarea <eyalde@jfrog.com>
2024-03-26 11:34:30 +0200

    remove test

--- artifactory_test.go
+++ artifactory_test.go
@@ -167,30 +167,6 @@ func TestArtifactorySimpleUploadSpec(t *testing.T) {
 	cleanArtifactoryTest()
 }
 
-func TestReleaseBundleImportOnPrem(t *testing.T) {
-	truebool := true
-	*tests.TestLifecycle = truebool
-
-	lcCli = coretests.NewJfrogCli(execMain, "jfrog", authenticateLifecycle())
-	cleanCallback := initLifecycleTest(t)
-	defer cleanCallback()
-
-	// Upload builds to create release bundles from.
-	deleteBuilds := uploadBuilds(t)
-	defer deleteBuilds()
-
-	createRbFromSpec(t, tests.LifecycleBuilds12, tests.LcRbName1, number1, true)
-
-	initArtifactoryTest(t, "")
-	wd, err := os.Getwd()
-	assert.NoError(t, err)
-
-	testFilePath := filepath.Join(wd, "testdata", "lifecycle", "import", "rb-import-test.zip")
-	artifactoryCliImport := coretests.NewJfrogCli(execMain, "jfrog", authenticate(false))
-	assert.NoError(t, artifactoryCliImport.Exec("rbi", testFilePath))
-	cleanArtifactoryTest()
-}
-
 func TestArtifactoryExcludeUpload(t *testing.T) {
 	testData := []struct {
 		uploadSpec string
