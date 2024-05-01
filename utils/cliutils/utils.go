13e417cfb63f17d15931539da467b8dc7f305520
Robi Nino <robi.nino@gmail.com>
2024-05-01 10:58:47 +0300

    Fix release bundle creation with project

--- utils/cliutils/utils.go
+++ utils/cliutils/utils.go
@@ -401,19 +401,32 @@ func handleSecretInput(c *cli.Context, stringFlag, stdinFlag string) (secret str
 	return commonCliUtils.HandleSecretInput(stringFlag, c.String(stringFlag), stdinFlag, c.Bool(stdinFlag))
 }
 
-func GetSpec(c *cli.Context, isDownload bool) (specFiles *speccore.SpecFiles, err error) {
+func GetSpec(c *cli.Context, isDownload, overrideFieldsIfSet bool) (specFiles *speccore.SpecFiles, err error) {
 	specFiles, err = speccore.CreateSpecFromFile(c.String("spec"), coreutils.SpecVarsStringToMap(c.String("spec-vars")))
 	if err != nil {
 		return nil, err
 	}
-	// Override spec with CLI options
+	trimPatternPrefixIfDownload(specFiles, isDownload)
+	overrideSpecFieldsIfNeeded(c, specFiles, overrideFieldsIfSet)
+	return
+}
+
+func overrideSpecFieldsIfNeeded(c *cli.Context, specFiles *speccore.SpecFiles, overrideFieldsIfSet bool) {
+	if !overrideFieldsIfSet {
+		return
+	}
 	for i := 0; i < len(specFiles.Files); i++ {
-		if isDownload {
-			specFiles.Get(i).Pattern = strings.TrimPrefix(specFiles.Get(i).Pattern, "/")
-		}
 		OverrideFieldsIfSet(specFiles.Get(i), c)
 	}
-	return
+}
+
+func trimPatternPrefixIfDownload(specFiles *speccore.SpecFiles, isDownload bool) {
+	if !isDownload {
+		return
+	}
+	for i := 0; i < len(specFiles.Files); i++ {
+		specFiles.Get(i).Pattern = strings.TrimPrefix(specFiles.Get(i).Pattern, "/")
+	}
 }
 
 func GetFileSystemSpec(c *cli.Context) (fsSpec *speccore.SpecFiles, err error) {
