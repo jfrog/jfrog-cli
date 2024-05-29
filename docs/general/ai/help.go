279061534ffe499544e175db6e9eab9855e96bed
Michael Sverdlov <michaelsv@jfrog.com>
2024-05-29 15:47:19 +0300

    Fix npm script
    Signed-off-by: Michael Sverdlov <michaelsv@jfrog.com>

--- docs/general/ai/help.go
+++ docs/general/ai/help.go
@@ -3,5 +3,5 @@ package ai
 var Usage = []string{"how"}
 
 func GetDescription() string {
-	return "This AI-based interface converts your natural language inputs into fully functional JFrog CLI commands."
+	return "This AI-based interface converts your natural language inputs into fully functional JFrog CLI commands. This is an interactive command that accepts no arguments."
 }
