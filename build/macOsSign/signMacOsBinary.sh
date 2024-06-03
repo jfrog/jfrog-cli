8fee265a0935868f4a19179e625764e8cf9a6105
delarea <eyalde@jfrog.com>
2024-06-03 14:19:59 +0300

    Refactor

--- build/macOsSign/signMacOsBinary.sh
+++ build/macOsSign/signMacOsBinary.sh
@@ -3,6 +3,7 @@
 # Assign environment variables to local variables
 # Set temp dir as runner temp dir
 TEMP_DIR=$RUNNER_TEMP
+KEYCHAIN_NAME="macos-build.keychain"
 
 # Validate input parameters
 if [ -z "$APPLE_CERT_DATA" ] || [ -z "$APPLE_CERT_PASSWORD" ] || [ -z "$APPLE_TEAM_ID" ] ; then
@@ -16,14 +17,14 @@ echo "$APPLE_CERT_DATA" | base64 --decode > "$TEMP_DIR"/certs.p12
 
 # Create a new keychain and set it as the default
 echo "Creating keychains..."
-security create-keychain -p "$APPLE_CERT_PASSWORD" macos-build.keychain
-security default-keychain -s macos-build.keychain
-security unlock-keychain -p "$APPLE_CERT_PASSWORD" macos-build.keychain
-security set-keychain-settings -t 3600 -u macos-build.keychain
+security create-keychain -p "$APPLE_CERT_PASSWORD" $KEYCHAIN_NAME
+security default-keychain -s $KEYCHAIN_NAME
+security unlock-keychain -p "$APPLE_CERT_PASSWORD" $KEYCHAIN_NAME
+security set-keychain-settings -t 3600 -u $KEYCHAIN_NAME
 
 # Import the certificate into the keychain
 echo "Importing certificate into keychain..."
-security import "$TEMP_DIR"/certs.p12 -k ~/Library/Keychains/macos-build.keychain -P "$APPLE_CERT_PASSWORD" -T /usr/bin/codesign
+security import "$TEMP_DIR"/certs.p12 -k ~/Library/Keychains/$KEYCHAIN_NAME -P "$APPLE_CERT_PASSWORD" -T /usr/bin/codesign
 
 # Verify the identity in the keychain
 echo "Verifying identity..."
@@ -31,8 +32,8 @@ security find-identity -p codesigning -v
 
 # Unlock the keychain to allow signing in terminal without asking for password
 echo "Unlocking the keychain"
-security unlock-keychain -p "$APPLE_CERT_PASSWORD" macos-build.keychain
-security set-key-partition-list -S apple-tool:,apple:, -s -k "$APPLE_CERT_PASSWORD" -D "$APPLE_TEAM_ID" -t private  macos-build.keychain
+security unlock-keychain -p "$APPLE_CERT_PASSWORD" $KEYCHAIN_NAME
+security set-key-partition-list -S apple-tool:,apple:, -s -k "$APPLE_CERT_PASSWORD" -D "$APPLE_TEAM_ID" -t private  $KEYCHAIN_NAME
 
 # Sign the binary
 echo "Signing the binary..."
@@ -41,3 +42,7 @@ codesign -s "$APPLE_TEAM_ID" --force jfrog-cli
 # Verify the binary is signed
 echo "Verifying binary is signed"
 codesign -vd ./jfrog-cli
+
+# Cleanup
+security delete-keychain $KEYCHAIN_NAME
+rm -rf "$TEMP_DIR"/certs.p12
