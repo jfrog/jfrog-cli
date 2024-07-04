6389c2a6344ef356a25866625f67ac93c4936a58
delarea <eyalde@jfrog.com>
2024-07-04 16:19:56 +0300

    Bump version from 1.0.0 to 2.63.0

---
+++ build/darwin-sign-and-notarize.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+# This script is used to sign and notarize a binary for MacOS. It consumes the following environment variables:
+#
+# APPLE_CERT_DATA: The base64 encoded Apple certificate data.
+# APPLE_CERT_PASSWORD: The password for the Apple certificate.
+# APPLE_TEAM_ID: The Apple Team ID.
+# APPLE_ACCOUNT_ID: The Apple Account ID.
+# APPLE_APP_SPECIFIC_PASSWORD: The app-specific password for the Apple account.
+# BINARY_FILE_NAME: The name of the binary file to be signed and notarized.
+#
+# APP_TEMPLATE_PATH: The path to the .app template used for notarization. It should have a specific structure:
+#                    - A Contents directory
+#                    - A MacOS directory inside the Contents directory which has to be empty
+#                    - An info.plist file inside the Contents directory
+# The name of the executable file should match the name of the .app folder.
+#
+# All of these environment variables should be currently in order for the process to work.
+
+validate_app_template_structure() {
+      if [ -z "$APP_TEMPLATE_PATH" ]; then
+          echo "Error: APP_TEMPLATE_PATH is not set."
+          return 1
+      fi
+
+      if [ ! -d "$APP_TEMPLATE_PATH" ]; then
+          echo "Error: $APP_TEMPLATE_PATH is not a directory."
+          return 1
+      fi
+
+      if [ ! -d "$APP_TEMPLATE_PATH/Contents" ]; then
+          echo "Error: Contents directory does not exist in $APP_TEMPLATE_PATH."
+          return 1
+      fi
+
+      if [ ! -d "$APP_TEMPLATE_PATH/Contents/MacOS" ]; then
+          echo "Error: MacOS directory does not exist in $APP_TEMPLATE_PATH/Contents."
+          return 1
+      fi
+
+      if [ ! -f "$APP_TEMPLATE_PATH/Contents/info.plist" ]; then
+          echo "Error: info.plist file does not exist in $APP_TEMPLATE_PATH/Contents."
+          return 1
+      fi
+
+      return 0
+  }
+validate_binary_name_and_app_template_path() {
+    # Extract the .app folder name from the APP_TEMPLATE_PATH
+    app_folder_name=$(basename "$APP_TEMPLATE_PATH" .app)
+
+    # Check if the BINARY_FILE_NAME is the same as the .app folder name
+    if [ "$BINARY_FILE_NAME" != "$app_folder_name" ]; then
+        echo "Error: The BINARY_FILE_NAME must match the .app folder name in APP_TEMPLATE_PATH."
+        return 1
+    fi
+
+    return 0
+}
+
+validateInputs(){
+  # Validate input parameters
+  if [ -z "$APPLE_CERT_DATA" ] || [ -z "$APPLE_CERT_PASSWORD" ] || [ -z "$APPLE_TEAM_ID" ] || [ -z "$BINARY_FILE_NAME" ] ; then
+      echo "Error: Missing environment variable."
+      exit 1
+  fi
+  # Validate the APP_TEMPLATE_PATH and BINARY_FILE_NAME has the same name.
+  validate_binary_name_and_app_template_path
+  # Validate app template structure
+  if ! validate_app_template_structure; then
+      echo "Error: The structure of APP_TEMPLATE_PATH is invalid. Please ensure it contains the following:"
+      echo "- ├── YOUR_APP.app
+              └── Contents
+                  └── MacOS
+                  └── info.plist"
+      echo "- A valid .app structure is needed in order to sign & notarize the binary"
+      exit 1
+  fi
+}
+
+# This function will prepare the keychain and certificate on the machine, needed for signing.
+prepare_keychain_and_certificate() {
+    # Assign environment variables to local variables
+    # Set temp dir as runner temp dir
+    TEMP_DIR=$RUNNER_TEMP
+    KEYCHAIN_NAME="macos-build.keychain"
+    # Save the decoded certificate data to a temporary file
+    echo "Saving Certificate to temp files"
+    echo "$APPLE_CERT_DATA" | base64 --decode > "$TEMP_DIR"/certs.p12
+    # Create a new keychain and set it as the default
+    echo "Creating keychains..."
+    security create-keychain -p "$APPLE_CERT_PASSWORD" $KEYCHAIN_NAME
+    security default-keychain -s $KEYCHAIN_NAME
+    security unlock-keychain -p "$APPLE_CERT_PASSWORD" $KEYCHAIN_NAME
+    security set-keychain-settings -t 3600 -u $KEYCHAIN_NAME
+
+    # Import the certificate into the keychain
+    echo "Importing certificate into keychain..."
+    security import "$TEMP_DIR"/certs.p12 -k ~/Library/Keychains/$KEYCHAIN_NAME -P "$APPLE_CERT_PASSWORD" -T /usr/bin/codesign
+
+    # Verify the identity in the keychain
+    echo "Verifying identity..."
+    security find-identity -p codesigning -v
+
+    # Unlock the keychain to allow signing in terminal without asking for password
+    echo "Unlocking the keychain"
+    security unlock-keychain -p "$APPLE_CERT_PASSWORD" $KEYCHAIN_NAME
+    security set-key-partition-list -S apple-tool:,apple:, -s -k "$APPLE_CERT_PASSWORD" -D "$APPLE_TEAM_ID" -t private  $KEYCHAIN_NAME
+}
+
+# Signs the binary file copies into the apple bundle template
+# The template is needed for notarizing the app
+sign_binary(){
+  # Sign the binary
+  echo "Signing the binary..."
+  codesign -s  "$APPLE_TEAM_ID"  --timestamp --deep --options runtime --force "$BINARY_FILE_NAME"
+}
+
+# Sends the app for notarization and staples the certificate to the app.
+# Binary files cannot be notarized as standalone files, they must be zipped and unzipped later on.
+notarize_app(){
+  # Move binary inside the app bundle template
+  mv "$BINARY_FILE_NAME" "$APP_TEMPLATE_PATH"/Contents/MacOS/"$BINARY_FILE_NAME"
+  # Zip it using ditto
+  ditto -c -k --keepParent ./build/jf.app ./jf-zipped
+  # Notarize the zipped app
+  xcrun notarytool submit jf-zipped --apple-id "$APPLE_ACCOUNT_ID" --team-id "$APPLE_TEAM_ID" --password "$APPLE_APP_SPECIFIC_PASSWORD"  --force --wait
+  # Staple ticket
+  xcrun stapler staple jf.app
+  # Unzip the extract the single binary file
+  unzip -o jf-zipped
+}
+
+cleanup(){
+  echo "Deleting keychain.."
+  security delete-keychain "$KEYCHAIN_NAME"
+  echo "Delete Certificate..."
+  rm -rf "$TEMP_DIR"/certs.p12
+}
+
+
+
+
+# Setup
+prepare_keychain_and_certificate
+# Sign & Notarize
+sign_binary
+notarize_app
+# Cleanup
+cleanup
+
