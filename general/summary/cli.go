9f2abba49729315f29ee9f113821c55f54cae757
delarea <eyalde@jfrog.com>
2024-08-13 16:50:43 +0300

    Add check before generating markdown

--- general/summary/cli.go
+++ general/summary/cli.go
@@ -2,39 +2,67 @@ package summary
 
 import (
 	"errors"
+	"fmt"
 	"github.com/jfrog/jfrog-cli-core/v2/artifactory/commands/commandssummaries"
 	"github.com/jfrog/jfrog-cli-core/v2/artifactory/utils"
 	"github.com/jfrog/jfrog-cli-core/v2/commandsummary"
 	commonCliUtils "github.com/jfrog/jfrog-cli-core/v2/common/cliutils"
 	coreConfig "github.com/jfrog/jfrog-cli-core/v2/utils/config"
+	"github.com/jfrog/jfrog-cli-core/v2/utils/coreutils"
 	"github.com/jfrog/jfrog-cli/utils/cliutils"
 	"github.com/jfrog/jfrog-client-go/utils/log"
 	"github.com/urfave/cli"
+	"os"
 )
 
-var markdownSections = []string{"security", "build-info", "upload"}
+type MarkdownSection string
+
+const (
+	Security  MarkdownSection = "security"
+	BuildInfo MarkdownSection = "build-info"
+	Upload    MarkdownSection = "upload"
+)
+
+var markdownSections = []MarkdownSection{Security, BuildInfo, Upload}
+
+func (ms MarkdownSection) String() string {
+	return string(ms)
+}
 
 func CreateSummaryMarkdown(c *cli.Context) (err error) {
-
+	// Verify that data has been recorded.
+	if os.Getenv(coreutils.OutputDirPathEnv) == "" {
+		return fmt.Errorf("cannot generate command summary: the output directory for command recording is not defined."+
+			" Please set the environment variable %s before executing your commands to view their summary", coreutils.OutputDirPathEnv)
+	}
+	// Get needed values to generate platform links.
 	serverUrl, majorVersion, err := extractServerUrlAndVersion(c)
 	if err != nil {
 		return err
 	}
-
+	// Invoke each section's Markdown generation.
 	for _, section := range markdownSections {
-		switch section {
-		case "security":
-		case "build-info":
-			buildInfoSummary, _ := commandsummary.New(commandssummaries.NewBuildInfoWithUrl(serverUrl, majorVersion), "build-info")
-			_ = buildInfoSummary.GenerateMarkdown()
-		case "upload":
-			uploadSummary, _ := commandsummary.New(commandssummaries.NewUploadSummary(serverUrl, majorVersion), "upload")
-			_ = uploadSummary.GenerateMarkdown()
-		default:
-			log.Warn("unknown section")
+		if err = generateSectionMarkdown(section, serverUrl, majorVersion); err != nil {
+			log.Warn("Failed to generate markdown for section %s: %v", section, err)
 		}
 	}
 	return
+}
+
+func generateSectionMarkdown(section MarkdownSection, serverUrl string, majorVersion int) error {
+	switch section {
+	case Security:
+		// Handle security section
+	case BuildInfo:
+		buildInfoSummary, _ := commandsummary.New(commandssummaries.NewBuildInfoWithUrl(serverUrl, majorVersion), string(BuildInfo))
+		return buildInfoSummary.GenerateMarkdown()
+	case Upload:
+		uploadSummary, _ := commandsummary.New(commandssummaries.NewUploadSummary(serverUrl, majorVersion), string(Upload))
+		return uploadSummary.GenerateMarkdown()
+	default:
+		return fmt.Errorf("unknown section: %s", section)
+	}
+	return nil
 }
 
 func createPlatformDetailsByFlags(c *cli.Context) (*coreConfig.ServerDetails, error) {
