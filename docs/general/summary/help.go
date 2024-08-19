bb1d13e44e388a1c4f1e4cf876555ae9abd88b03
delarea <eyalde@jfrog.com>
2024-08-19 13:37:55 +0300

    Edit help

--- docs/general/summary/help.go
+++ docs/general/summary/help.go
@@ -3,5 +3,7 @@ package summary
 var Usage = []string{"csm"}
 
 func GetDescription() string {
-	return `Generates a Summary in markdown format of recorded CLI commands.`
+	return `Generates a Summary of recorded CLI commands there were executed on the current machine.
+	The report is generated in Markdown format and saved in the root directory of JFROG_CLI_COMMAND_SUMMARY_OUTPUT_DIR.
+`
 }
