e77ed8976f15740f6c54a6710844bad3f2275c34
Bhanu Reddy <bhanur@jfrog.com>
2025-06-23 01:47:37 +0530

    Fixed artifactory url

--- sonarintegration_test.go
+++ sonarintegration_test.go
@@ -101,7 +101,7 @@ func authenticateEvidence(isBuildPublish bool) string {
 		evidenceDetails.ArtifactoryUrl = *tests.JfrogUrl + "artifactory"
 		evidenceDetails.Url = *tests.JfrogUrl
 	}
-	cred = fmt.Sprintf("--url=%s ----artifactory-url=%s", *tests.JfrogUrl, *tests.JfrogUrl+"artifactory")
+	cred = fmt.Sprintf("--url=%s --artifactory-url=%s", *tests.JfrogUrl, *tests.JfrogUrl+"artifactory")
 	if *tests.JfrogAccessToken != "" {
 		evidenceDetails.AccessToken = *tests.JfrogAccessToken
 		cred = fmt.Sprintf("%s --access-token=%s", cred, evidenceDetails.AccessToken)
