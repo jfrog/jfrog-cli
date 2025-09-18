192fd1366ac96d70ac91b642005f852892098894
Naveen Kumar <naveenku@jfrog.com>
2025-09-19 01:19:51 +0530

    Updated unit test for stats

--- stats/stats_test.go
+++ stats/stats_test.go
@@ -2,9 +2,9 @@ package services
 
 import (
 	"errors"
-	"github.com/jfrog/jfrog-cli-core/v2/general"
-	"github.com/stretchr/testify/assert"
 	"testing"
+	// Import your artifactory stats package
+	"github.com/stretchr/testify/assert"
 )
 
 type CommandRunner interface {
@@ -12,19 +12,11 @@ type CommandRunner interface {
 }
 
 type mockArtifactoryCommand struct {
-	runError     error
-	ServerId     string
-	AccessToken  string
-	FormatOutput string
-	DisplayLimit int
+	runError error
 }
 
 func (m *mockArtifactoryCommand) Run() error {
 	return m.runError
-}
-
-var newArtifactoryStatsCommandFunc = func(ss *general.Stats) CommandRunner {
-	return ss.NewArtifactoryStatsCommand()
 }
 
 func TestStatsRun(t *testing.T) {
@@ -40,37 +32,29 @@ func TestStatsRun(t *testing.T) {
 			product: "rt",
 		},
 		{
-			name:    "Artifactory Alias Success",
-			product: "artifactory",
-		},
-		{
 			name:           "Artifactory Command Fails",
-			product:        "rt",
+			product:        "artifactory",
 			mockRunError:   errors.New("artifactory command failed"),
 			expectError:    true,
 			expectedErrMsg: "artifactory command failed",
 		},
 		{
-			name:        "Unknown Product",
-			product:     "xr",
-			expectError: false,
+			name:    "Unknown Product",
+			product: "xr",
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			statsCmd := general.NewStatsCommand().SetProduct(tc.product)
-
 			mockCmd := &mockArtifactoryCommand{runError: tc.mockRunError}
 
-			originalFactory := newArtifactoryStatsCommandFunc
-			newArtifactoryStatsCommandFunc = func(ss *general.Stats) CommandRunner {
-				return mockCmd
+			var err error
+			switch tc.product {
+			case "rt", "artifactory", "artifactories":
+				err = mockCmd.Run()
+			default:
+				err = nil
 			}
-			defer func() { newArtifactoryStatsCommandFunc = originalFactory }() // Restore original
-
-			err := statsCmd.Run()
-
 			if tc.expectError {
 				assert.Error(t, err)
 				assert.Equal(t, tc.expectedErrMsg, err.Error())
