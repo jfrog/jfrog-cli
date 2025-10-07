e7a4f09c4acf06e89c7474704a49db89db60888e
Naveen Kumar <naveenku@jfrog.com>
2025-10-07 14:33:21 +0530

    Update docs/general/stats/help.go
    Co-authored-by: Bhanu Reddy <bhanur@jfrog.com>

--- docs/general/stats/help.go
+++ docs/general/stats/help.go
@@ -17,7 +17,7 @@ func GetArguments() string {
 	--format (optional)
 	The output format in which you want statistics to be shown. Currently, Json, Table and Text (default) are supported.
 
-	--accessToken(optional)
+	--access-token(optional)
 	The access token using which you want statistics will be fetched from jfrog instance. By default, logged user access token is used. For some products, like JPDs, projects, user needs to provide admin token.
 `
 }
