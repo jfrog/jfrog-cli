8d3867f733a7e94b10f2f89f322ff40de0869774
Naveen Kumar <naveenku@jfrog.com>
2025-10-07 14:34:37 +0530

    Update stats/stats.go
    Co-authored-by: Bhanu Reddy <bhanur@jfrog.com>

--- stats/stats.go
+++ stats/stats.go
@@ -24,7 +24,7 @@ func GetStats(c *cli.Context) error {
 			SetFormat(format)
 		return newStatsCommand.Run()
 	} else {
-		_ = cli.ShowSubcommandHelp(c)
+		cli.ShowSubcommandHelp(c)
 		return fmt.Errorf("wrong product %s only artifactory or rt is supported", productName))
 		return nil
 	}
