c1dfb0a42cbf7ef7a821a072edbea29dacb9ca4c
Naveen Kumar <naveenku@jfrog.com>
2025-11-20 12:57:15 +0530

    Added native helm commands docs

---
+++ docs/buildtools/helmcommand/help.go
@@ -0,0 +1,19 @@
+package helmcommand
+
+var Usage = []string{"helm <helm arguments> [command options]"}
+
+func GetDescription() string {
+	return "Run native Helm command."
+}
+
+func GetArguments() string {
+	return `	install                  Install a chart.
+	upgrade                  Upgrade a release.
+	package                  Package a chart directory into a chart archive.
+	push                     Push a chart to remote.
+	pull                     Download a chart from remote.
+	repo                     Add, list, remove, update, and index chart repositories.
+	dependency               Manage a chart's dependencies.
+	help, h                  Show help for any command.`
+}
+
