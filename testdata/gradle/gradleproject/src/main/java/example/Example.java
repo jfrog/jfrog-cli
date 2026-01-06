612e1402e77fd13359f2c4287ec3484f17ebb98f
nitinp19 <nitinp@jfrog.com>
2026-01-06 16:31:20 +0530

    nitpic changes

--- testdata/gradle/gradleproject/src/main/java/example/Example.java
+++ testdata/gradle/gradleproject/src/main/java/example/Example.java
@@ -1,11 +1,4 @@
 package example;
-
-/**
- * Simple example class for the gradle test project.
- * Note: We intentionally don't use JUnit here because dependency resolution
- * may not work when the JFrog Gradle plugin configures the resolver to use
- * the test Artifactory repository (which may not have JUnit cached).
- */
 public class Example {
     public boolean isTrue() {
         return true;
